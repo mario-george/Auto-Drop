32249f6687e7fd659cdeaf23c7c13ae0d525a6df
mario-george <vortexspades@gmail.com>
2024-01-23 00:20:04 +0200

    Add Poppins font to fonts.tsx

--- Auto Drop/components/ui/fonts.tsx
+++ Auto Drop/components/ui/fonts.tsx
@@ -1,4 +1,5 @@
-import { Inter, Cairo } from "next/font/google";
+import { Inter, Cairo, Poppins } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 export const cairo = Cairo({ subsets: ["arabic"] });
