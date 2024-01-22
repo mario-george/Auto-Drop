835d44f1d077bbb71e21939ee6c9bc6f3221b1df
mario-george <vortexspades@gmail.com>
2024-01-23 00:20:35 +0200

    Add SideNav component to dashboard page

--- Auto Drop/app/[locale]/(dashboards)/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/page.tsx
@@ -3,10 +3,11 @@
 import { store } from "@/store";
 import { Button } from "@/components/ui/button";
 import { userActions } from "@/store/user-slice";
-
+import SideNav from "@/components/layout/layout";
 export default function Home() {
   return (
     <>
+      <SideNav />
       <div className="text-center">Dahboard &ldquo;Protected&rdquo;</div>
       <Button
         onClick={() => store.dispatch(userActions.logout())}
