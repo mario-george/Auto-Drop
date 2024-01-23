c9ac32839c93e2906b6f0ac893fcbfa8aa1da2f1
mario-george <vortexspades@gmail.com>
2024-01-23 17:25:37 +0200

    Add ClientHeader component to DashboardLayout

--- Auto Drop/app/[locale]/(dashboards)/layout.tsx
+++ Auto Drop/app/[locale]/(dashboards)/layout.tsx
@@ -1,9 +1,21 @@
 import DashboardProtectWrapper from "@/components/dashboard-protect-wrapper";
+import { useTranslations } from "next-intl";
+import ClientHeader from "@/app/[locale]/(dashboards)/_components/shared/ClientHeader";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DashboardProtectWrapper>{children}</DashboardProtectWrapper>;
+  const t = useTranslations("ClientTopBar");
+
+  return (
+    <DashboardProtectWrapper>
+      <div className="bg-[#F0F3F4] py-4">
+        <ClientHeader lang={t("lang")} />
+
+        {children}
+      </div>
+    </DashboardProtectWrapper>
+  );
 }
