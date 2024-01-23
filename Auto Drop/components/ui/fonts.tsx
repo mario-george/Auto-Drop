7f856a77722051793c6c420b08f01d5afbb7174f
mario-george <vortexspades@gmail.com>
2024-01-23 20:31:05 +0200

    Update font imports with fallback options

--- Auto Drop/components/ui/fonts.tsx
+++ Auto Drop/components/ui/fonts.tsx
@@ -1,5 +1,15 @@
 import { Inter, Cairo, Poppins } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"] });
-export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
-export const cairo = Cairo({ subsets: ["arabic"] });
+export const inter = Inter({
+  subsets: ["latin"],
+  fallback: ["Arial", "sans-serif"],
+});
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  fallback: ["Arial", "sans-serif"],
+});
+export const cairo = Cairo({
+  subsets: ["arabic"],
+  fallback: ["Arial", "sans-serif"],
+});
