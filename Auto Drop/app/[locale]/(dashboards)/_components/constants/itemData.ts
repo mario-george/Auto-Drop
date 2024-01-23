aa67fea3b8479c7c4c51f1b19b4ede40185ec19c
mario-george <vortexspades@gmail.com>
2024-01-23 17:24:57 +0200

    Refactor iconData in itemData.ts

--- Auto Drop/app/[locale]/(dashboards)/_components/constants/itemData.ts
+++ Auto Drop/app/[locale]/(dashboards)/_components/constants/itemData.ts
@@ -1,3 +1,4 @@
+
 import NavBarSVG from "@/components/icons/ClientSVGs/NavBarSVG";
 import ContactSVG from "@/components/icons/ClientSVGs/ContactSVG";
 import CartSVG from "@/components/icons/ClientSVGs/CartSVG";
@@ -9,6 +10,8 @@ import SettingsSVG from "@/components/icons/ClientSVGs/SettingsSVG";
 import PlansSVG from "@/components/icons/ClientSVGs/PlansSVG";
 import LinkingSVG from "@/components/icons/ClientSVGs/LinkingSVG";
 import OrdersSVG from "@/components/icons/ClientSVGs/OrdersSVG";
+import {useTranslations} from 'next-intl'
+
 
 type SvgComponent =
   | typeof NavBarSVG
@@ -25,20 +28,22 @@ type SvgComponent =
 
 interface IconDataItem {
   icon: SvgComponent;
-  text?: string;
+  text: string;
+  route: string;
 }
 
-const iconData: IconDataItem[] = [
-  { icon: HomePageSVG, text: "Main Page" },
-  { icon: ProductsSVG, text: "Products" },
-  { icon: OwnedProductsSVG, text: "My Products" },
-  { icon: CartSVG, text: "Cart" },
-  { icon: OrdersSVG, text: "Orders" },
-  { icon: PlansSVG, text: "Plans" },
-  { icon: LinkingSVG, text: "Linking" },
-  { icon: SettingsSVG, text: "Settings" },
-  { icon: WalletSVG, text: "Wallet" },
-  { icon: ContactSVG, text: "Contact" },
-];
-
-export default iconData;
+ 
+  export const iconData: IconDataItem[] = [
+    { icon: HomePageSVG, text: "homepage", route: "/home" },
+    { icon: ProductsSVG, text: "products", route: "/products" },
+    { icon: OwnedProductsSVG, text: "myProducts", route: "/my-products" },
+    { icon: CartSVG, text: "cart", route: "/cart" },
+    { icon: OrdersSVG, text: "orders", route: "/orders" },
+    { icon: PlansSVG, text: "plans", route: "/plans" },
+    { icon: LinkingSVG, text: "linking", route: "/linking" },
+    { icon: SettingsSVG, text: "settings", route: "/settings" },
+    { icon: WalletSVG, text: "wallet", route: "/wallet" },
+    { icon: ContactSVG, text: "contact", route: "/contact" },
+  ];
+  
+  
