1778c44284485c2474edd2c76dcba1c1d23d6710
mario-george <vortexspades@gmail.com>
2024-01-23 17:24:46 +0200

    Refactor dashboard page component

--- Auto Drop/app/[locale]/(dashboards)/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/page.tsx
@@ -1,20 +1,6 @@
-"use client";
-
-import { store } from "@/store";
-import { Button } from "@/components/ui/button";
-import { userActions } from "@/store/user-slice";
-import SideNav from "@/components/layout/layout";
+import LayoutWrapper from './_components/shared/LayoutWrapper'
 export default function Home() {
   return (
-    <>
-      <SideNav />
-      <div className="text-center">Dahboard &ldquo;Protected&rdquo;</div>
-      <Button
-        onClick={() => store.dispatch(userActions.logout())}
-        className="bg-slate-800 mx-auto block hover:bg-white hover:text-slate-800 hover:border hover:border-slate-800"
-      >
-        Logout
-      </Button>
-    </>
+<LayoutWrapper/>
   );
 }
