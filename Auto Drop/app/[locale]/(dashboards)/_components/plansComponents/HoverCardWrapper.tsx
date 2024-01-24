d77daac29322112d644d732e57923a6435879465
mario-george <vortexspades@gmail.com>
2024-01-24 16:28:45 +0200

    Refactor HoverCardWrapper component

--- Auto Drop/app/[locale]/(dashboards)/_components/plansComponents/HoverCardWrapper.tsx
+++ Auto Drop/app/[locale]/(dashboards)/_components/plansComponents/HoverCardWrapper.tsx
@@ -20,14 +20,18 @@ export default function HoverCardWrapper({
 }:HoverCardProps) {
   return (
     <HoverCard>
-      <HoverCardTrigger className="w-auto min-h-auto">
+      <HoverCardTrigger className="relative flex flex-col flex-wrap bg-white rounded-xl shadow-lg m-auto max-[350px]:w-[100%] max-[426px]:w-[47%] w-[48%] lg:w-[20.5%] py-2 md:py-7 md:pt-11">
         {children}
       </HoverCardTrigger>
-      <HoverCardContent dir={`${locale == "ar" ? "rtl" : "ltr"}`}>
-        <div className="flex flex-col min-w-[44.3125rem]">
-          {title}
+      <HoverCardContent dir={`${locale == "ar" ? "rtl" : "ltr"}`} >
+        <div className="flex flex-col min-w-[44.3125rem] font-bold text-[24px] ">
+          <span className="font-bold text-[24px] mb-1 shadow">
 
-          <div className="flex items-center space-x-2">
+          {title}
+          </span>
+<div className="">
+
+          <div className="flex items-center space-s-2 mb-5">
             <span>{remainingProducts}</span>
             <Progress
               dir={`${locale == "ar" ? "rtl" : "ltr"}`}
@@ -35,10 +39,15 @@ export default function HoverCardWrapper({
               className="w-[60%]"
             />
           </div>
-          <div className="flex">
+          <div className="flex items-center space-s-2">
             <span>{remainingOrders}</span>
-            bar
+            <Progress
+              dir={`${locale == "ar" ? "rtl" : "ltr"}`}
+              value={33}
+              className="w-[60%]"
+            />
           </div>
+</div>
           <div className=""></div>
         </div>
       </HoverCardContent>
