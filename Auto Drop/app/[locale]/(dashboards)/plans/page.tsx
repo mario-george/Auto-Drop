ff6768db647a42d3c31bad6e37128b39670193f6
mario-george <vortexspades@gmail.com>
2024-01-24 15:04:35 +0200

    Add monthly plans component and translations

--- Auto Drop/app/[locale]/(dashboards)/plans/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/plans/page.tsx
@@ -1,5 +1,33 @@
+import Monthly from "../_components/plansComponents/Packages";
+import { useLocale, useTranslations } from "next-intl";
+
 export default function PlansPage() {
-  return (
-<div/>
+  const t = useTranslations("packages");
+  const t2 = useTranslations("clientPlans");
+  const locale = useLocale();
+
+  return (<>
+  
+  <Monthly
+    locale={locale}
+
+    monthly={t("monthly")}
+    PackagePro={t("Package Pro")}
+    PackagePlus={t("Package Plus")}
+    BasicPackage={t("Basic Package")}
+    subscribtion={t("subscribtion")}
+    SAR={t("SAR")}
+    remainingProducts={t2("remainingProducts")}
+    remainingOrders={t2("remainingOrders")}
+    subscriptionDate={t2("subscriptionDate")}
+    subscriptionExpirationDate={t2("subscriptionExpirationDate")}
+    plansTitle={t2("plansTitle")}
+    productsNumber={t2("productsNumber")}
+    ordersNumber={t2("ordersNumber")}
+    tryForFree={t2("tryForFree")}
+
+    
+  />
+  </>
   );
 }
