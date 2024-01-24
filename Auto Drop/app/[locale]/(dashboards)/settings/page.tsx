46633dd8246b64dc3959889e58f3c7ffd619a3ef
mario-george <vortexspades@gmail.com>
2024-01-24 16:32:11 +0200

    Add settings page component

---
+++ Auto Drop/app/[locale]/(dashboards)/settings/page.tsx
@@ -0,0 +1,11 @@
+import { useLocale, useTranslations } from "next-intl";
+
+export default function SettingsPage() {
+  const t = useTranslations("settings");
+  const locale = useLocale();
+
+  return (<>
+  
+  </>
+  );
+}
