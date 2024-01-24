dbc796618ce08d75511f6952b4c296308e4d78c9
mario-george <vortexspades@gmail.com>
2024-01-24 16:29:51 +0200

    Refactor progress bar animation and styling

--- Auto Drop/components/ui/progress.tsx
+++ Auto Drop/components/ui/progress.tsx
@@ -11,7 +11,9 @@ const Progress = React.forwardRef<
   React.useEffect(() => {
     // Animate progress from 0 to the actual value
     const interval = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress < value ? prevProgress + 1 : value));
+      setProgress((prevProgress) =>
+        prevProgress < value ? prevProgress + 1 : value || 0
+      );
     }, 20);
 
     // Clean up interval on unmount
@@ -31,13 +33,27 @@ const Progress = React.forwardRef<
     >
       <ProgressPrimitive.Indicator
         className="h-full w-full flex-1 transition-all overflow-hidden  "
-        style={{ transform: dir === "rtl" ? `translateX(${100 - progress}%)` : `translateX(-${100 - progress}%)` }}
+        style={{
+          transform:
+            dir === "rtl"
+              ? `translateX(${100 - progress}%)`
+              : `translateX(-${100 - progress}%)`,
+        }}
       >
         <div className="h-full w-full bg-[#b29e84] rounded-md  "></div>
       </ProgressPrimitive.Indicator>
-      <div className={`absolute  top-0 bottom-0   bg-[#eaecec] z-[100] rounded-r-lg ${ dir=="rtl"?`left-[90%] right-0 ` :`left-0 right-[90%]`}`}  ></div>
-      <div className={`absolute top-[20%] bottom-[10%]  bg-[#b29e84] z-[101] h-3 p-1 ${ dir=="rtl"?`rounded-r-lg  left-[90%] right-[1%] ` :`rounded-l-lg  left-[1%] right-[90%]`} `}></div>
-
+      <div
+        className={`absolute  top-0 bottom-0   bg-[#eaecec] z-[100] rounded-r-lg ${
+          dir == "rtl" ? `left-[90%] right-0 ` : `left-0 right-[90%]`
+        }`}
+      ></div>
+      <div
+        className={`absolute top-[20%] bottom-[10%]  bg-[#b29e84] z-[101] h-3 p-1 ${
+          dir == "rtl"
+            ? `rounded-r-lg  left-[90%] right-[1%] `
+            : `rounded-l-lg  left-[1%] right-[90%]`
+        } `}
+      ></div>
     </ProgressPrimitive.Root>
   );
 });
