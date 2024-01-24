6c6d44d3b29f6fd108c0ec3bc8a005cf01cc319a
mario-george <vortexspades@gmail.com>
2024-01-24 15:06:13 +0200

    Refactor HoverCardWrapper component

--- Auto Drop/app/[locale]/(dashboards)/_components/plansComponents/HoverCardWrapper.tsx
+++ Auto Drop/app/[locale]/(dashboards)/_components/plansComponents/HoverCardWrapper.tsx
@@ -1,28 +1,47 @@
 import {
-    HoverCard,
-    HoverCardContent,
-    HoverCardTrigger,
-  } from "@/components/ui/hover-card";
-import { Progress } from "@/components/ui/progress"
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+interface HoverCardProps {
+  title: string;
+  remainingProducts: string;
+  remainingOrders: string;
+  children: React.ReactNode;
+  locale: string;
+}
+export default function HoverCardWrapper({
+  title,
+  remainingProducts,
+  remainingOrders,
+  children,
+  locale,
+}:HoverCardProps) {
+  return (
+    <HoverCard>
+      <HoverCardTrigger className="w-auto min-h-auto">
+        {children}
+      </HoverCardTrigger>
+      <HoverCardContent dir={`${locale == "ar" ? "rtl" : "ltr"}`}>
+        <div className="flex flex-col min-w-[44.3125rem]">
+          {title}
 
-export default function HoverCardWrapper({title,remainingProducts,remainingOrders,children,locale}){
-    return   <HoverCard>
-    <HoverCardTrigger className="w-auto min-h-auto">{children}</HoverCardTrigger>
-    <HoverCardContent dir={`${locale == "ar" ? "rtl" : "ltr"}`}
-    >
-      <div className="flex flex-col min-w-[44.3125rem]">
-      {title}
-
-        <div className="flex items-center space-x-2">
-<span>{remainingProducts}</span>
-<Progress dir={`${locale == "ar" ? "rtl" : "ltr"}`}value={33} className="w-[60%]"/>
+          <div className="flex items-center space-x-2">
+            <span>{remainingProducts}</span>
+            <Progress
+              dir={`${locale == "ar" ? "rtl" : "ltr"}`}
+              value={33}
+              className="w-[60%]"
+            />
+          </div>
+          <div className="flex">
+            <span>{remainingOrders}</span>
+            bar
+          </div>
+          <div className=""></div>
         </div>
-        <div className="flex">
-<span>{remainingOrders}</span>
-bar
-        </div>
-        <div className=""></div>
-      </div>
-    </HoverCardContent>
-  </HoverCard>
+      </HoverCardContent>
+    </HoverCard>
+  );
 }
