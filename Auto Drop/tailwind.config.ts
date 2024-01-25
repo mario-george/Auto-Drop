24b591443913229c70551285c0257a597fcc8ed0
mario-george <vortexspades@gmail.com>
2024-01-25 16:16:43 +0200

    Update tailwind.config.ts with new screen sizes

--- Auto Drop/tailwind.config.ts
+++ Auto Drop/tailwind.config.ts
@@ -1,14 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        ms: "321px",
+        mm: "376px",
+        ml: "426px",
+        tab: "769px",
+        lap: "1025px",
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
