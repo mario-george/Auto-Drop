640708a2f6bc72ee8037f9ff7310faa46314467e
mario-george <vortexspades@gmail.com>
2024-01-27 18:17:37 +0200

    Add HeaderContainer component

---
+++ Auto Drop/app/[locale]/(dashboards)/_components/shared/HeaderContainer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+interface CardHeader {
+  title: string;
+  icon?: any;
+  IconComponent?: React.ComponentType;
+  className?:string
+}
+export default function HeaderContainer({
+  title,
+  icon,
+  IconComponent,className
+}: CardHeader) {
+  return (
+    <div className={`bg-white  !mx-6 text-[#253439]  px-6 py-2 my-12 rounded-lg shadow  ${className? className :null}`}>
+      {icon && <Image src={icon} width={24} height={24} alt="icon" />}
+      {IconComponent ? 
+      <div className="flex  items-center space-x-4">
+      <IconComponent />
+        <div>{title}</div>
+      </div>
+      :<div className="tab:mx-3">{title}</div>
+      }
+
+    </div>
+  );
+}
