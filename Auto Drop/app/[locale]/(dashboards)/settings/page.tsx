e55b86aa22b294e1f1b9cbe8119d37d12d717438
mario-george <vortexspades@gmail.com>
2024-01-28 12:16:25 +0200

    Add settings page with localization support

--- Auto Drop/app/[locale]/(dashboards)/settings/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/settings/page.tsx
@@ -1,8 +1,30 @@
 import { useLocale, useTranslations } from "next-intl";
+import HeaderContainer from "../_components/shared/HeaderContainer";
+import { Button } from "@/components/ui/button";
+import Settings from "./_components/Settings";
 
 export default function SettingsPage() {
-  const t = useTranslations("settings");
+  const t = useTranslations("clientSettings");
   const locale = useLocale();
 
-  return <></>;
+  return (
+    <>
+      <Settings
+        settings={t("settings")}
+        currentPassword={t("currentPassword")}
+        newPassword={t("newPassword")}
+        confirmPassword={t("confirmPassword")}
+        saveChanges={t("saveChanges")}
+        changePassword={t("changePassword")}
+        changeAccountDetails={t("changeAccountDetails")}
+        merchantID={t("merchantID")}
+        name={t("name")}
+        marketName={t("marketName")}
+        marketLink={t("marketLink")}
+        email={t("email")}
+        phone={t("phone")}
+        country={t("country")}
+      />{" "}
+    </>
+  );
 }
