8b7adba0fc7ccb5e40ed9196d516f02e9441daa2
mario-george <vortexspades@gmail.com>
2024-01-28 12:16:37 +0200

    Refactor HeaderContainer component

--- Auto Drop/app/[locale]/(dashboards)/_components/shared/HeaderContainer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/_components/shared/HeaderContainer.tsx
@@ -3,24 +3,29 @@ interface CardHeader {
   title: string;
   icon?: any;
   IconComponent?: React.ComponentType;
-  className?:string
+  className?: string;
 }
 export default function HeaderContainer({
   title,
   icon,
-  IconComponent,className
+  IconComponent,
+  className,
 }: CardHeader) {
   return (
-    <div className={`bg-white  !mx-6 text-[#253439]  px-6 py-2 my-12 rounded-lg shadow  ${className? className :null}`}>
+    <div
+      className={`bg-white  !mx-6 text-[#253439]  px-6 py-2 my-12 rounded-lg shadow  ${
+        className ? className : null
+      }`}
+    >
       {icon && <Image src={icon} width={24} height={24} alt="icon" />}
-      {IconComponent ? 
-      <div className="flex  items-center space-x-4">
-      <IconComponent />
-        <div>{title}</div>
-      </div>
-      :<div className="tab:mx-3">{title}</div>
-      }
-
+      {IconComponent ? (
+        <div className="flex  items-center space-s-4">
+          <IconComponent />
+          <div>{title}</div>
+        </div>
+      ) : (
+        <div className="tab:mx-3">{title}</div>
+      )}
     </div>
   );
 }
