adaec53c7fca9d73ac3ef8b9bda5355c006e4d1c
mario-george <vortexspades@gmail.com>
2024-01-28 13:40:31 +0200

    Add client-side code for authentication

--- Auto Drop/app/[locale]/(dashboards)/link-account/_components/AvailblePlatforms.tsx
+++ Auto Drop/app/[locale]/(dashboards)/link-account/_components/AvailblePlatforms.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 export default function AvailablePlatforms({
@@ -7,7 +8,8 @@ export default function AvailablePlatforms({
   className,
   cardClassName,
   soonButtonClasses,
-  connectButtonClasses,store
+  connectButtonClasses,
+  store,
 }: {
   soon: string;
   linkButton: string;
@@ -18,18 +20,10 @@ export default function AvailablePlatforms({
   connectButtonClasses?: string;
   store?: boolean;
 }) {
-  /*   const cards = [
-    {
-      image: "/salla.svg",
-      alt: "salla",
-      circleLink: true,
-      imageW: 222,
-      imageH: 125,
-    },
-    { image: "/shopify.svg", alt: "shopify", imageW: 240, imageH: 131 },
-    { image: "/client/wix.svg", alt: "wix", imageW: 240, imageH: 131 },
-    { image: "/client/twilio.svg", alt: "twilio", imageW: 240, imageH: 131 },
-  ]; */
+  const authHandler = async (link: string) => {
+    const url = process.env.NEXT_PUBLIC_BACK_URL + link;
+    window.location.href = url;
+  };
   return (
     <>
       <div className={``}>
@@ -42,7 +36,11 @@ export default function AvailablePlatforms({
                 key={index}
                 className={`bg-white rounded-lg  flex flex-col justify-center items-center flex-1 pt-6 ${cardClassName}`}
               >
-                <div className={`${card.circleLink &&store&& `pb-6 mm:pb-10 lap:pt-6`}`}>
+                <div
+                  className={`${
+                    card.circleLink && store && `pb-6 mm:pb-10 lap:pt-6`
+                  }`}
+                >
                   <Image
                     width={card.imageW}
                     height={card.imageH}
@@ -54,6 +52,11 @@ export default function AvailablePlatforms({
                 {card.circleLink ? (
                   <Button
                     className={`min-w-full  bg-[#253439] hover:bg-[#253439]  !rounded-t-none ${connectButtonClasses}`}
+                    onClick={() => {
+                      if (card.authLink) {
+                        authHandler(card.authLink);
+                      }
+                    }}
                   >
                     <div className="flex justify-center items-center cursor-pointer space-s-2  ">
                       <Image
