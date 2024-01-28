8136b1ceaff4253be7860d1617aa6f1ccc8d25e3
mario-george <vortexspades@gmail.com>
2024-01-28 12:16:48 +0200

    Refactor link-account page to use AvailableStores and AvailablePlatforms components

--- Auto Drop/app/[locale]/(dashboards)/link-account/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/link-account/page.tsx
@@ -1,25 +1,68 @@
 import HeaderContainer from "../_components/shared/HeaderContainer";
-import CardsSection from "./_components/CardsSection";
+import CardsSection from "./_components/AvailableStores";
 import { useLocale, useTranslations } from "next-intl";
-import Platforms from '@/components/icons/clientPages/Platforms'
-import CartSVG  from '@/components/icons/clientPages/CartSVG';
+import Platforms from "@/components/icons/clientPages/Platforms";
+import CartSVG from "@/components/icons/clientPages/CartSVG";
+import AvailablePlatforms from "./_components/AvailblePlatforms";
 export default function LinkAccount() {
   const locale = useLocale();
   const t = useTranslations("home");
   const t2 = useTranslations("linkAccount");
+  const PlatformCards = [
+    {
+      image: "/salla.svg",
+      alt: "salla",
+      circleLink: true,
+      imageW: 222,
+      imageH: 125,
+    },
+    { image: "/shopify.svg", alt: "shopify", imageW: 240, imageH: 131 },
+    { image: "/client/wix.svg", alt: "wix", imageW: 240, imageH: 131 },
+    { image: "/client/twilio.svg", alt: "twilio", imageW: 240, imageH: 131 },
+  ];
+  const StoreCards = [
+    {
+      image: "/aliexpress.svg",
+      alt: "aliexpress",
+      circleLink: true,
+      imageW: 212.5,
+      imageH: 42.28,
+    },
+    { image: "/amazon.svg", alt: "amazon", imageW: 188.65, imageH: 99.94 },
+    { image: "/cj.svg", alt: "cj", imageW: 116.08, imageH: 99.94 },
+  ];
+  return (
+    <>
+      <HeaderContainer
+        IconComponent={Platforms}
+        title={t2("availablePlatforms")}
+        className=" "
+      />
+      <AvailablePlatforms
+        linkButton={t("linkButton")}
+        soon={t("soon")}
+        Cards={PlatformCards}
+      />
 
-  return (<>
-  <HeaderContainer  IconComponent={Platforms} title={t2('availablePlatforms')}  className=" "/>
-       <CardsSection
+      <HeaderContainer IconComponent={CartSVG} title={t2("availableStores")} />
+      {/*       <CardsSection
         locale={locale}
         linkHeader={t("linkHeader")}
         subLink1={t("subLink1")}
         subLink2={t("subLink2")}
         linkButton={t("linkButton")}
         soon={t("soon")}
+      /> */}
+      <AvailablePlatforms
+        linkButton={t("linkButton")}
+        soon={t("soon")}
+        Cards={StoreCards}
+        className="lap:max-w-[67.5%]"
+        cardClassName={`pt-12`}
+        soonButtonClasses="mt-6"
+        connectButtonClasses="mt- mt-auto"
+        store={true}
       />
-  <HeaderContainer IconComponent={CartSVG} title={t2('availableStores')}/>
-
-  </>
+    </>
   );
 }
