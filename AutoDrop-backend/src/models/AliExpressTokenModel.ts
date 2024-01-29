f090d5f1da3395c224c834c34316f2dc248cc5f2
mario-george <vortexspades@gmail.com>
2024-01-29 12:41:19 +0200

    Add required constraint to userId field in AliExpressTokenModel and SallaTokenModel

--- AutoDrop-backend/src/models/AliExpressTokenModel.ts
+++ AutoDrop-backend/src/models/AliExpressTokenModel.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const AliExpressTokenSchema = new mongoose.Schema({
   accessToken: { type: String, required: true },
   refreshToken: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const AliExpressToken = mongoose.model(
--- AutoDrop-backend/src/models/SallaTokenModel.ts
+++ AutoDrop-backend/src/models/SallaTokenModel.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const SallaTokenSchema = new mongoose.Schema({
   accessToken: { type: String, required: true },
   refreshToken: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const SallaToken = mongoose.model("SallaToken", SallaTokenSchema);
