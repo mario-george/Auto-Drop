079b30466d177f1c8bc04ff3aab597b25dbfb22b
mario-george <vortexspades@gmail.com>
2024-01-29 13:35:17 +0200

    Update authentication and cookie settings

--- AutoDrop-backend/src/index.ts
+++ AutoDrop-backend/src/index.ts
@@ -38,7 +38,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
       httpOnly: true,
     },
   })
