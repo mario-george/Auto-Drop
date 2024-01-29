4366558a5478f7eb383d8a3e79ec2f5410c9ef70
mario-george <vortexspades@gmail.com>
2024-01-30 00:00:35 +0200

    remove cursor-pointer

--- Auto Drop/app/[locale]/(dashboards)/link-account/_components/AvailblePlatforms.tsx
+++ Auto Drop/app/[locale]/(dashboards)/link-account/_components/AvailblePlatforms.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
+import { motion } from "framer-motion";
 
 export default function AvailablePlatforms({
   soon,
@@ -14,6 +15,7 @@ export default function AvailablePlatforms({
   connectButtonClasses,
   store,
   linkButtonConnected,
+  locale,
 }: {
   soon: string;
   linkButton: string;
@@ -24,6 +26,7 @@ export default function AvailablePlatforms({
   connectButtonClasses?: string;
   store?: boolean;
   linkButtonConnected?: string;
+  locale?: string;
 }) {
   const sallaToken = useSelector((state: RootState) => state.user.sallaToken);
   const aliExpressToken = useSelector(
@@ -36,9 +39,13 @@ export default function AvailablePlatforms({
     const url = process.env.NEXT_PUBLIC_BACK_URL + link;
     window.location.href = url;
   };
+  const variants = {
+    hidden: { opacity: 0, x: locale === "ar" ? 50 : -50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+  };
   return (
     <>
-      <div className={``}>
+      <motion.div initial="hidden" animate="visible" variants={variants}>
         <div
           className={`flex flex-wrap mm:mx-3 ml:px-6 tab:max-w-full max-w-[90%] w-full tab:space-s-8 tab:mx-0 space-y-6 tab:space-y-0 flex-col tab:flex-row justify-center items-stretch ${className}`}
         >
@@ -105,7 +112,7 @@ export default function AvailablePlatforms({
                   <Button
                     className={` hover:bg-neutral-200  min-w-full !rounded-t-none  bg-neutral-200 cursor-auto !py-[1.2rem]  tab:!py-[1.3rem] ${soonButtonClasses}`}
                   >
-                    <div className="flex justify-center items-center cursor-pointer space-s-2">
+                    <div className="flex justify-center items-center space-s-2">
                       <div className=" text-neutral-400 text-[20px]">
                         {soon}
                       </div>
@@ -116,7 +123,7 @@ export default function AvailablePlatforms({
             );
           })}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
