1fab957914aa2796847aced43b387b948b66a998
mario-george <vortexspades@gmail.com>
2024-01-30 18:25:40 +0200

    Add variants for disabled state in tailwind.config.js

--- Auto Drop/tailwind.config.js
+++ Auto Drop/tailwind.config.js
@@ -80,6 +80,12 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      variants: {
+        extend: {
+          opacity: ["disabled"],
+          cursor: ["disabled"],
+        },
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("tailwindcss-rtl")],
