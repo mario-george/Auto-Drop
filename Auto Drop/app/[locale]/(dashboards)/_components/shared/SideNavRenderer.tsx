955f08293a661a2e32955dcc9013b693be815b9b
mario-george <vortexspades@gmail.com>
2024-02-02 22:22:42 +0200

    Refactor SideNavRenderer component to use responsive design

--- Auto Drop/app/[locale]/(dashboards)/_components/shared/SideNavRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/_components/shared/SideNavRenderer.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 import LogoutHandler from "./LogoutHandler";
 import { useMediaQuery } from "react-responsive";
 import { useState, useEffect } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import NavComponent from "./SideNavList";
 
 export default function SideNavRenderer({
   iconInfo,
@@ -20,22 +22,11 @@ export default function SideNavRenderer({
   whatsappMsg: string;
   locale: string;
 }) {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   const isMediumScreen = useMediaQuery({
     query: "(min-width: 769px) and (max-width: 1024px)",
   });
   const isLargeScreen = useMediaQuery({ query: "(min-width: 1025px)" });
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
   let marginDirection = locale === "ar" ? "mr-3" : "ml-3";
   let marginDirection2 = locale === "ar" ? "mr-4" : "ml-3";
   let marginDirection3 = locale === "ar" ? "mr-2" : "ml-1";
@@ -43,10 +34,6 @@ export default function SideNavRenderer({
     locale === "ar" ? "translate-x-[5px]" : "-translate-x-1";
   const path = usePathname();
   const [isNavOpen, setIsNavOpen] = useState(isSmallScreen ? true : false);
-
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
 
   const [navClass, setNavClass] = useState("");
   const [applyPadding, setApplyPadding] = useState(false);
@@ -161,60 +148,33 @@ export default function SideNavRenderer({
         </div>
 
         <nav className="flex flex-col justify-between ">
-          <ul className="flex flex-col mx-2 space-y-2 !mb-auto">
-            {iconData.map((link, index) => {
-              let isActive = link.route == path;
-              return (
-                <motion.li
-                  className={`group icon-path flex items-center py-2 px-4 hover:bg-[#F0F3F400] text-opacity-50  ${
-                    isActive
-                      ? "bg-[#F0F3F4] !icon-path-active shadow-md !text-opacity-100 icon "
-                      : ""
-                  } text-[#253439] hover:text-opacity-100 hover:bg-[#f0f3f4] rounded-lg hover:shadow-md`}
-                  key={index}
-                >
-                  <Link href={link.route} className="flex items-center">
-                    <link.icon />
-                    <motion.span
-                      className={`${marginDirection} whitespace-nowrap`}
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                    >
-                      {iconInfo[index]}
-                    </motion.span>
-                  </Link>
-                </motion.li>
-              );
-            })}
-          </ul>
-          <div className="flex flex-col  mx-2 space-y-6  border-t h-full mt-[100%]">
-            <LogoutHandler
-              logoutMsg={logoutMsg}
-              marginDirection={marginDirection2}
-            />
-            <motion.li
-              className={`flex items-center py-2 px-1 ${
-                locale == "en" ? "ml-1" : "mr-1"
-              } text-[#253439] hover:text-black  rounded-lg hover:shadow-md border border-[#00A859]`}
-            >
-              <Link href={"/"} className="flex items-center">
-                <Image
-                  width={42}
-                  height={30}
-                  src={"/client/whatsapp.svg"}
-                  alt="whatsapp"
-                  className={`transform ${whatsappTranslate}`}
-                />
-                <motion.span
-                  className={`${marginDirection3} whitespace-nowrap`}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                >
-                  {whatsappMsg}
-                </motion.span>
-              </Link>
-            </motion.li>
-          </div>
+          {isSmallScreen ? (
+            <>
+              <ScrollArea className="h-[150vw]">
+                <div className="z-[9999999]" dir={isAr ? "rtl" : "ltr"}>
+                  <NavComponent
+                    iconData={iconData}
+                    iconInfo={iconInfo}
+                    logoutMsg={logoutMsg}
+                    whatsappMsg={whatsappMsg}
+                    locale={locale}
+                    isNavOpen={isNavOpen}
+                  />
+                </div>
+              </ScrollArea>
+            </>
+          ) : (
+            <>
+              <NavComponent
+                iconData={iconData}
+                iconInfo={iconInfo}
+                logoutMsg={logoutMsg}
+                whatsappMsg={whatsappMsg}
+                locale={locale}
+                isNavOpen={isNavOpen}
+              />
+            </>
+          )}
         </nav>
       </motion.div>
       {isAr && (
