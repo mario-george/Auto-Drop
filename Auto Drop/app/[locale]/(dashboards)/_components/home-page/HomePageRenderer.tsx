7538235dcf89bad817d73f62f1c801a7cf99acfa
mario-george <vortexspades@gmail.com>
2024-02-03 16:28:11 +0200

    Add HomePageRenderer component

---
+++ Auto Drop/app/[locale]/(dashboards)/_components/home-page/HomePageRenderer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function HomePageRenderer(props: any) {
+  const { t } = props;
+  let prodNotLinked = t("prodNotLinked");
+  let numOfSales = t("numOfSales");
+
+  let numOfOrders = t("numOfOrders");
+
+  let platform = t("Platform");
+
+  let prodLinked = t("prodLinked");
+
+  let numOfProd = t("numOfProd");
+
+  let goodMorning = t("gm");
+
+  let totalProfits = t("tp");
+
+  let profits = t("p");
+  let wallet = t("w");
+  let latestRequests = t("lr");
+  let CustomerName = t("cn");
+  let orderStatus = t("os");
+  let date = t("d");
+  let amount = t("a");
+  let details = t("d2");
+  let done = t("done");
+  let beingPaid = t("being-paid");
+  let canceled = t("Canceled");
+
+  return <div></div>;
+}
