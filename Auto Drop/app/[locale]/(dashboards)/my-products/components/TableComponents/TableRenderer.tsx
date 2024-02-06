8f383f3307b6d53761c18429ff0bc3b6b743c98c
mario-george <vortexspades@gmail.com>
2024-02-06 22:09:43 +0200

    Remove unused import and schema definition

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/TableRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/TableRenderer.tsx
@@ -3,7 +3,6 @@ import path from "path";
 import { z } from "zod";
 import Cols from "./components/columns";
 import { DataTable } from "./components/data-table";
-import { taskSchema } from "./data/schema";
 import ColsExtract from "./ColumnsExtractor";
 
 // Simulate a database read for tasks.
