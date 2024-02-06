3fb58e57a30922450d28cab4c92601b43c178ca0
mario-george <vortexspades@gmail.com>
2024-02-06 22:05:21 +0200

    Add localization and table components to my-products page

--- Auto Drop/app/[locale]/(dashboards)/my-products/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/page.tsx
@@ -1,5 +1,24 @@
+import { useTranslations } from "next-intl";
 import React from "react";
-
+import Header from "./components/TableComponents/components/Header";
+import TableRenderer from "./components/TableComponents/TableRenderer";
 export default function page() {
-  return <div></div>;
+  const t = useTranslations("myProducts");
+
+  return (
+    <>
+      <Header title={t("products")} />
+      <TableRenderer
+        productName={t("prodName")}
+        sellPrice={t("sellPrice")}
+        category={t("category")}
+        platform={t("platform")}
+        inventory={t("inv")}
+        searchByProd={t("searchByProd")}
+        unAvProd={t("unAvProd")}
+        price={t("price")}
+        unUpProd={t("unUpProd")}
+      />
+    </>
+  );
 }
