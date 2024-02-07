c663835e7cc7b0dec4687725d19c066432977a5b
mario-george <vortexspades@gmail.com>
2024-02-07 14:23:54 +0200

    Refactor DataTablePagination component

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/data-table-pagination.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/data-table-pagination.tsx
@@ -25,20 +25,12 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
+    <div className="flex items-center justify-center px-2">
       <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-          Page {table.getState().pagination.pageIndex + 1} of{" "}
-          {table.getPageCount()}
-        </div>
-
         <div className="flex space-s-3">
           <Pagination>
-            <PaginationContent>
+            <PaginationContent className="space-s-4 mx-auto">
+              <PaginationItem></PaginationItem>
               {Array.from({ length: table.getPageCount() }, (_, i) => {
                 return i + 1;
               }).map((page) => {
@@ -48,55 +40,27 @@ export function DataTablePagination<TData>({
                       isActive={
                         table.getState().pagination.pageIndex + 1 === page
                       }
+                      className={`shadow-md ${
+                        table.getState().pagination.pageIndex + 1 === page
+                          ? `bg-[#253439] text-white`
+                          : ``
+                      }`}
+                      onClick={() => table.setPageIndex(page - 1)}
                     >
                       {page}
                     </PaginationLink>
                   </PaginationItem>
                 );
-              })}
-              <PaginationItem>
-                <PaginationNext href="#" />
-              </PaginationItem>
+              })}{" "}
+              <Button
+                onClick={() => table.nextPage()}
+                className="bg-white text-[#253439] hover:bg-white/90 shadow-md"
+                disabled={!table.getCanNextPage()}
+              >
+                Next
+              </Button>
             </PaginationContent>
           </Pagination>
-        </div>
-        <div className="flex items-center space-x-2">
-          <Button
-            variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <span className="sr-only">Go to first page</span>
-            <DoubleArrowLeftIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <span className="sr-only">Go to previous page</span>
-            <ChevronLeftIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            <span className="sr-only">Go to next page</span>
-            <ChevronRightIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            disabled={!table.getCanNextPage()}
-          >
-            <span className="sr-only">Go to last page</span>
-            <DoubleArrowRightIcon className="h-4 w-4" />
-          </Button>
         </div>
       </div>
     </div>
