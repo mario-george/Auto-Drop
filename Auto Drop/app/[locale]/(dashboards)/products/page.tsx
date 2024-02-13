a9c7ca27f8a35b9e2ba4f59e0d6d597291fd844d
mario-george <vortexspades@gmail.com>
2024-02-13 18:00:15 +0200

    Add localization and components to products page

--- Auto Drop/app/[locale]/(dashboards)/products/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/page.tsx
@@ -1,5 +1,34 @@
+import { useLocale, useTranslations } from "next-intl";
 import React from "react";
 
+import ProductsRenderer from "./_components/ProductsRenderer";
+import MotionWrapper from "../_components/shared/MotionWrapper";
 export default function Page() {
-  return <div></div>;
+  const t = useTranslations("productsPage");
+  const locale = useLocale();
+  return (
+    <>
+      <MotionWrapper locale={locale}>
+        <div className="px-3">
+        
+          <ProductsRenderer locale={locale}  categories={t("categories")}
+            allProducts={t("allProducts")}
+            searchByProd={t("searchByProd")} shops={t("shops")} />
+        </div>
+      </MotionWrapper>
+      {/*   <Header title={t("products")} />
+      <TableRenderer
+        productName={t("prodName")}
+        sellPrice={t("sellPrice")}
+        category={t("category")}
+        platform={t("platform")}
+        inventory={t("inv")}
+        searchByProd={t("searchByProd")}
+        unAvProd={t("unAvProd")}
+        price={t("price")}
+        unUpProd={t("unUpProd")}
+        locale={locale}
+      /> */}
+    </>
+  );
 }
