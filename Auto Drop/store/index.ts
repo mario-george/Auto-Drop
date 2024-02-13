aa70ddab9ccaec1069bdfc536c40dac78eb7110d
mario-george <vortexspades@gmail.com>
2024-02-13 18:01:53 +0200

    Add productsSliceReducer to rootReducer

--- Auto Drop/store/index.ts
+++ Auto Drop/store/index.ts
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import { storage } from "./customStorage";
 import userSlice from "./user-slice";
+import { productsSliceReducer } from "./productsSlice";
 
 const persistConfig = {
   key: "root",
@@ -10,6 +11,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: persistReducer(persistConfig, userSlice.reducer),
+  products: productsSliceReducer,
 });
 
 export const store = configureStore({
