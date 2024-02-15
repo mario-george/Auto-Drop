d60668fc70fb75913a589b0c8fd8c4d7aa3f5356
mario-george <vortexspades@gmail.com>
2024-02-15 21:46:58 +0200

    Add Spinner and FetchSpinner components

--- Auto Drop/app/[locale]/(dashboards)/products/_components/ProductsSpinner.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/_components/ProductsSpinner.tsx
@@ -1,7 +1,15 @@
-function Spinner() {
+export function Spinner() {
   return (
     <div className="flex items-center justify-center min-h-full mx-auto min-w-screen ">
       <div className="animate-spin h-[20rem] w-[20rem] border-t-2 border-b-2 border-blue-500 rounded-full"></div>
+    </div>
+  );
+}
+
+export function FetchSpinner(){
+  return (
+    <div className="z-30 absolute inset-0 mx-auto my-auto bg-white/50 ">
+      <div className="z-30 absolute inset-0 mx-auto my-auto animate-spin h-[15rem] w-[15rem] border-t-2 border-b-2 border-blue-500 rounded-full"></div>
     </div>
   );
 }
