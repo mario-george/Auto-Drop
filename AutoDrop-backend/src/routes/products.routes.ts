5d26d93be25a0d057c0223ac9e39b700f94e5ef6
mario-george <vortexspades@gmail.com>
2024-02-15 21:46:34 +0200

    Add GetProductShippingDetailsByID route

--- AutoDrop-backend/src/routes/products.routes.ts
+++ AutoDrop-backend/src/routes/products.routes.ts
@@ -5,11 +5,12 @@ import {
   GetRecommendedProducts,
   GetRecommendedProductsPost,
 } from "../controllers/aliexpress/GetProducts.controller";
+import { GetProductShippingDetailsByID } from "../controllers/aliexpress/products/shipping/GetProductsShipping";
 const router = Router();
 router.get("/products", GetRecommendedProducts);
 router.post("/products", GetRecommendedProductsPost);
 router.get("/productsByName", GetProductByName);
 router.post("/getProductDetails", GetProductDetails);
-
+router.post("/getShippingDetails", GetProductShippingDetailsByID);
 
 export default router;
