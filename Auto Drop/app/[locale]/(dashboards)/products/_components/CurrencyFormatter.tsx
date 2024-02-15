3286bbfe3e6ba403e82a4b13320a51d4a4c6905e
mario-george <vortexspades@gmail.com>
2024-02-15 21:46:28 +0200

    Add CurrencyFormatterShippingInfo function

--- Auto Drop/app/[locale]/(dashboards)/products/_components/CurrencyFormatter.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/_components/CurrencyFormatter.tsx
@@ -12,3 +12,17 @@ export default function CurrencyFormatter(
 
   return new Intl.NumberFormat("en-SA", properties).format(value);
 }
+export  function CurrencyFormatterShippingInfo(
+  value: number,
+  options?: Intl.NumberFormatOptions
+): string {
+  const properties = {
+    style: "currency",
+    currency: "SAR",
+    currencyDisplay: "code",
+    currencySign: "standard",
+    ...options,
+  } satisfies Intl.NumberFormatOptions;
+
+  return new Intl.NumberFormat("en-SA", properties).format(value);
+}
