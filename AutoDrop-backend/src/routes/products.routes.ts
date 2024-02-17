e0247ee0e4dd051b3975abac89a83ab4c957fc14
mario-george <vortexspades@gmail.com>
2024-02-17 11:02:47 +0200

    Add product creation route and authentication middleware

--- AutoDrop-backend/src/routes/products.routes.ts
+++ AutoDrop-backend/src/routes/products.routes.ts
@@ -6,11 +6,20 @@ import {
   GetRecommendedProductsPost,
 } from "../controllers/aliexpress/GetProducts.controller";
 import { GetProductShippingDetailsByID } from "../controllers/aliexpress/products/shipping/GetProductsShipping";
+import Authentication from "../assits/Authentication";
+import { CreateProductController } from "../controllers/aliexpress/products/productCRUD/CRUD";
+import { CheckValidationSchema } from "../validate/CheckValidation";
+import { CreateProduct } from "../validate/products";
 const router = Router();
 router.get("/products", GetRecommendedProducts);
 router.post("/products", GetRecommendedProductsPost);
 router.get("/productsByName", GetProductByName);
 router.post("/getProductDetails", GetProductDetails);
 router.post("/getShippingDetails", GetProductShippingDetailsByID);
+router.post(
+  "/product/create",
+  [Authentication(), ...CreateProduct, CheckValidationSchema],
+  CreateProductController
+);
 
 export default router;
