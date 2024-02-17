6afa68e207d90031928b640e71300e011661481a
mario-george <vortexspades@gmail.com>
2024-02-17 11:03:10 +0200

    Update user authentication logic

--- Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListAR.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListAR.tsx
@@ -24,36 +24,143 @@ export default function ProductsListAR({
   shippingInfoPending,
   commissionV,
   setCommissionV,
-  addCommissionHandler,lang
+  addCommissionHandler,
+  lang,
 }: any) {
   return (
     <>
       {" "}
-      <MotionWrapper locale='ar'>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 gap-x-3 overflow-hidden">
-            {productsAR?.map((product: any, i: number) => {
-              return (
-                <Card
-                  className="relative flex flex-col !p-0 my-3 shadow-md rounded-lg justify-between overflow-hidden"
-                  key={i}
+      <MotionWrapper locale="ar">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 gap-x-3 overflow-hidden">
+          {productsAR?.map((product: any, i: number) => {
+            let shippingInfoActive =
+              productsShippingInfo &&
+              productsShippingInfo.length == productsAR.length &&
+              productsShippingInfo[i] &&
+              productsShippingInfo[i][0] &&
+              productsShippingInfo[i][0].activated;
+            console.log(shippingInfoActive);
+            console.log(shippingInfoPending);
+            return (
+              <Card
+                className="relative flex flex-col !p-0 my-3 shadow-md rounded-lg justify-between overflow-hidden"
+                key={i}
+              >
+                {productsShippingInfo &&
+                  productsShippingInfo[i] &&
+                  productsShippingInfo[i][0].loading === "pending" && (
+                    <FetchSpinner />
+                  )}
+                {productsShippingInfo &&
+                  productsShippingInfo[i] &&
+                  productsShippingInfo[i][0].activated && (
+                    <>
+                      <MotionWrapperExit locale="en">
+                        <div className="text-[#253439] " dir="rtl">
+                          <div className="mx-auto bg-[#f0f3f4] text-center pt-16 p-6   ">
+                            معلومات و طرق الشحن للمنتج{" "}
+                          </div>
+                          <ScrollArea className="h-[18rem]">
+                            {productsShippingInfo[i].map(
+                              (shipping: any, ind: number) => {
+                                if (shipping.noShipping) {
+                                  return (
+                                    <div
+                                      key={ind}
+                                      className="flex flex-col space-y-2 pl-2 mt-6"
+                                    >
+                                      <div className="flex space-s-3">
+                                        <span>نوع الشحن:</span>
+                                        <span className="text-[#008767]">
+                                          لا يوجد شحن متوفر.
+                                        </span>
+                                      </div>
+                                    </div>
+                                  );
+                                }
+
+                                return (
+                                  <>
+                                    <div dir="rtl"
+                                      key={ind}
+                                      className="flex flex-col space-y-2 pl-2 mt-6 "
+                                    >
+                                      <div className="flex space-s-3">
+                                        <span>نوع الشحن:</span>
+                                        <span className="text-[#008767]">
+                                          {
+                                            productsShippingInfo[i][0]
+                                              .shippingType
+                                          }{" "}
+                                        </span>
+                                      </div>
+                                      <div className="flex space-s-3">
+                                        {" "}
+                                        <span>المدة:</span>{" "}
+                                        <span className="text-[#008767]">
+                                          {productsShippingInfo[i][0].duration}
+                                        </span>
+                                      </div>
+                                      <div className="flex space-s-3">
+                                        {" "}
+                                        <span>الربح بعد خصم الشحن: </span>{" "}
+                                        <span className="text-[#008767]">
+                                          {CurrencyFormatterShippingInfo(
+                                            productsShippingInfo[i][0]
+                                              .profitAfterDiscount
+                                          )}
+                                        </span>{" "}
+                                      </div>
+                                      <div className="flex space-s-3 text-[#C1121F]">
+                                        {" "}
+                                        <span>السعر:</span>{" "}
+                                        <span>
+                                          {CurrencyFormatter(
+                                            productsShippingInfo[i][0].price
+                                          )}
+                                        </span>{" "}
+                                      </div>
+                                      {ind !==
+                                        productsShippingInfo[i].length - 1 && (
+                                        <Separator />
+                                      )}
+                                    </div>
+                                  </>
+                                );
+                              }
+                            )}
+                          </ScrollArea>
+                        </div>
+                      </MotionWrapperExit>
+                    </>
+                  )}
+                <div
+                  className={cn(
+                    "absolute top-[1rem] ",
+                    locale == "ar" ? `right-[1rem]` : `left-[1rem]`
+                  )}
                 >
                   <div
-                    className={cn(
-                      "absolute top-[1rem] ",
-                      locale == "ar" ? `right-[1rem]` : `left-[1rem]`
-                    )}
+                    className="overflow-hidden"
+                    onClick={() => {
+                      if (
+                        product.checked &&
+                        (product.commission || product.commission === 0)
+                      ) {
+                        toggleShoppingCartActivated(i);
+                      }
+                    }}
                   >
-                    <div className="overflow-hidden">
-                      <Image
-                        src={`/client/products/shoppingCart.svg`}
-                        className=" rounded-full cursor-pointer "
-                        height={45}
-                        width={45}
-                        alt="shoppingCart"
-                      />
-                    </div>
+                    <Image
+                      src={`/client/products/shoppingCart.svg`}
+                      className=" rounded-full cursor-pointer "
+                      height={45}
+                      width={45}
+                      alt="shippingCart"
+                    />
                   </div>
-
+                </div>
+                {!shippingInfoActive && (
                   <div
                     className={
                       (cn("absolute top-[10rem] left-[6rem]"),
@@ -70,6 +177,8 @@ export default function ProductsListAR({
                       )}
                     />
                   </div>
+                )}
+                {!shippingInfoActive && (
                   <div className="">
                     <Image
                       src={
@@ -81,6 +190,43 @@ export default function ProductsListAR({
                       alt="aliexpressProduct"
                     />
                   </div>
+                )}
+
+                {product.checked && !shippingInfoActive ? (
+                  <>
+                    <div className="space-y-3 flex flex-col pt-7">
+                      <span className="mx-auto">من فضلك أدخل عمولتك. </span>
+                      <div className="flex space-x-3 items-center justify-between px-3 pb-2">
+                        <Button
+                          className="bg-blue-500 text-white hover:bg-blue-600 rounded-full "
+                          onClick={() => {
+                            addCommissionHandler(i, product, commissionV[i]);
+                          }}
+                          disabled={shippingInfoPending}
+                        >
+                          <BiSend />
+                        </Button>
+                        <div className="relative mt-auto">
+                          <Input
+                            type="number"
+                            className="pr-6"
+                            value={commissionV[i]}
+                            onChange={(e: any) => {
+                              const newValues = [...commissionV];
+                              newValues[i] = e.target.value;
+                              setCommissionV(newValues);
+                            }}
+                          />
+                          <span className="absolute inset-y-0 right-0 pr-2 flex items-center text-gray-500">
+                            %
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : shippingInfoActive ? (
+                  <></>
+                ) : (
                   <div className="p-3 flex flex-col  gap-y-3">
                     <div
                       className={`flex justify-between gap-x-2 items-center`}
@@ -149,10 +295,12 @@ export default function ProductsListAR({
                       </a>
                     </div>
                   </div>
-                </Card>
-              );
-            })}
-          </div></MotionWrapper>
+                )}
+              </Card>
+            );
+          })}
+        </div>
+      </MotionWrapper>
     </>
   );
 }
