6215e8d2f072eeccb688c3b5d6a31dee04e7a684
mario-george <vortexspades@gmail.com>
2024-02-20 23:25:36 +0200

    Add DeleteProductById controller for deleting a product

---
+++ AutoDrop-backend/src/controllers/aliexpress/products/productCRUD/DeleteProduct.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../../../utils/catchAsync";
+import { Product } from "../../../../models/product.model";
+export const DeleteProductById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log('yes')
+    if (!req.params) {
+      return res
+        .status(400)
+        .json({ message: "Missing productId in query parameters." });
+    }
+    //@ts-ignore
+    let { productId }: { productId: string } = req.params;
+
+    let product = await Product.findOne({
+      //@ts-ignore
+      merchant: req.user._id as any,
+      _id: productId,
+    });
+    if (!product) {
+      console.log("No product found");
+      return res.status(404).json({ message: "Product Not Found." });
+    }
+    await Product.deleteOne({ _id: product._id });
+    return res.json({ message: "Product deleted" });
+  }
+);
