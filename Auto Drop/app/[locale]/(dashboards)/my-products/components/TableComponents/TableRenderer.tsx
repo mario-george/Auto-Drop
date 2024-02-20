5631179ed810cf3837b5064fd27a5b44cad459ac
mario-george <vortexspades@gmail.com>
2024-02-20 03:09:35 +0200

    Refactor TableRenderer component and add ProductsFetch component

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/TableRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/TableRenderer.tsx
@@ -4,17 +4,18 @@ import { z } from "zod";
 import Cols from "./components/columns";
 import { DataTable } from "./components/data-table";
 import ColsExtract from "./ColumnsExtractor";
-
+import axiosInstance from "../../../_components/shared/AxiosInstance";
+import ProductsFetch from './ProductsFetch'
 // Simulate a database read for tasks.
 async function getMyProducts() {
-  const data = await fs.readFile(
+  /* const data = await fs.readFile(
     path.join(
       process.cwd(),
       "app/[locale]/(dashboards)/my-products/components/TableComponents/data/myproducts.json"
     )
-  );
-
-  return JSON.parse(data.toString());
+  ); */
+  const data2 = await axiosInstance.get("/aliexpress/product/getProducts");
+  return JSON.parse(data2.toString());
 }
 interface myProductProps {
   productName: string;
@@ -29,13 +30,19 @@ interface myProductProps {
   locale: string;
 }
 export default async function TaskPage(props: myProductProps) {
-  const myProducts = await getMyProducts();
-
+  const getMyProductsData = async () => {
+    const data2 = await axiosInstance.get("/aliexpress/product/getProducts");
+    return JSON.parse(data2.toString());
+  };
+  const myProducts: any = [];
   return (
     <>
+    <ProductsFetch {...props}>
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex bg-white">
         <ColsExtract {...props} myProducts={myProducts} />
       </div>
+    </ProductsFetch>
+
     </>
   );
 }
