4a52489e44b5861c70b1fc676c087cd8866da0ed
mario-george <vortexspades@gmail.com>
2024-02-20 03:08:25 +0200

    Refactor ButtonsRenderer component and add linkProductHandler

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/ButtonsRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/ButtonsRenderer.tsx
@@ -1,11 +1,22 @@
+import axiosInstance from "@/app/[locale]/(dashboards)/_components/shared/AxiosInstance";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-export default function ButtonsRenderer() {
-  let buttonClassD = "rounded-full bg-[#c1121f] px-2 py-2"
-  let buttonClassL = "rounded-full bg-[#008767] px-2 py-2"
-  let buttonClassE = "rounded-full bg-[#253439] px-2 py-2"
-  let buttonClassS = "rounded-full bg-[#f79042] px-2 py-2"
-    return (
+export default function ButtonsRenderer({ id }: any) {
+  console.log("idddd");
+  console.log(id);
+  let buttonClassD = "rounded-full bg-[#c1121f] px-2 py-2";
+  let buttonClassL = "rounded-full bg-[#008767] px-2 py-2";
+  let buttonClassE = "rounded-full bg-[#253439] px-2 py-2";
+  let buttonClassS = "rounded-full bg-[#f79042] px-2 py-2";
+
+  let linkProductHandler = async () => {
+    const res = await axiosInstance.post(
+      "aliexpress/product/linkProductSalla",
+      { productId: id }
+    );
+    console.log(res.data)
+  };
+  return (
     <div className="flex flex-row-reverse gap-3">
       <Button className={buttonClassD}>
         <Image
@@ -14,34 +25,32 @@ export default function ButtonsRenderer() {
           width={24}
           height={24}
         />
-      </Button><Button  className={buttonClassL}>
-<Image
-        src={`/client/my-products/link.svg`}
-        alt={`link`}
-        width={24}
-        height={24}
-      />
-</Button>
+      </Button>
+      <Button className={buttonClassL} onClick={linkProductHandler}>
+        <Image
+          src={`/client/my-products/link.svg`}
+          alt={`link`}
+          width={24}
+          height={24}
+        />
+      </Button>
       <Button className={buttonClassE}>
-      <Image
-        src={`/client/my-products/edit.svg`}
-        alt={`edit`}
-        width={24}
-        height={24}
-      />
-        </Button>
-       
-    
-      
- <Button className={buttonClassS}>
-      <Image
-        src={`/client/my-products/store.svg`}
-        alt={`store`}
-        width={24}
-        height={24}
-      />
-</Button>
+        <Image
+          src={`/client/my-products/edit.svg`}
+          alt={`edit`}
+          width={24}
+          height={24}
+        />
+      </Button>
 
+      <Button className={buttonClassS}>
+        <Image
+          src={`/client/my-products/store.svg`}
+          alt={`store`}
+          width={24}
+          height={24}
+        />
+      </Button>
     </div>
   );
 }
