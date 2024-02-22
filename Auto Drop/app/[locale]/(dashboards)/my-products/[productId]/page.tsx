a1e56dec04722c8b17ebc362bd1542e207932bc3
mario-george <vortexspades@gmail.com>
2024-02-22 18:55:02 +0200

    Refactor ProductEdit component to fetch and display product information
    This commit refactors the `ProductEdit` component to fetch product information from the server using `axiosInstance` and displays the fetched data. It also handles error cases and displays an appropriate message.

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/page.tsx
@@ -1,10 +1,44 @@
-import { useRouter, useSearchParams } from "next/navigation";
-export default function ProductEdit() {
-  const router = useRouter();
+"use client";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../_components/shared/AxiosInstance";
+import ProductEditHeader from "./_components/ProductEditHeader";
+import MotionWrapperExit from "../../_components/shared/MotionWrapperExit";
+import { useLocale } from "next-intl";
 
-  const params = useSearchParams();
+export default function ProductEdit(props: { params: { productId: string } }) {
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState(false);
+  const locale = useLocale();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `/aliexpress/product/getProductInfo/${props.params.productId}`
+        );
+        console.log(response);
+        setProduct(response.data.product);
+      } catch (error) {
+        console.error("Failed to fetch product:", error);
+        setError(true);
+      }
+    };
 
-  const productId = params.get("productId");
-
-  return <></>;
+    fetchData();
+  }, [props.params.productId]);
+  if (error) {
+    return <div>No Product found with this id</div>;
+  }
+  console.log(product);
+  return (
+    <>
+      <MotionWrapperExit locale={locale}>
+        <ProductEditHeader />
+        <div>{JSON.stringify(product)}</div>
+        <div>
+          Product Id is
+          {props.params.productId}
+        </div>
+      </MotionWrapperExit>
+    </>
+  );
 }
