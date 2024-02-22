e6980f4e39f6e1660289e3fa6be2b936f6c0c15b
mario-george <vortexspades@gmail.com>
2024-02-22 19:35:25 +0200

    Refactor ButtonsRenderer component

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/ButtonsRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/ButtonsRenderer.tsx
@@ -1,9 +1,11 @@
 import axiosInstance from "@/app/[locale]/(dashboards)/_components/shared/AxiosInstance";
 import { Button } from "@/components/ui/button";
+import { useLocale } from "next-intl";
 import Image from "next/image";
-export default function ButtonsRenderer({ id,setMyProducts }: any) {
-  console.log("idddd");
-  console.log(id);
+import { useRouter } from "next/navigation";
+export default function ButtonsRenderer({ id, setMyProducts }: any) {
+  const router = useRouter();
+  const locale = useLocale();
   let buttonClassD = "rounded-full bg-[#c1121f] px-2 py-2";
   let buttonClassL = "rounded-full bg-[#008767] px-2 py-2";
   let buttonClassE = "rounded-full bg-[#253439] px-2 py-2";
@@ -22,15 +24,20 @@ export default function ButtonsRenderer({ id,setMyProducts }: any) {
     );
     if (res.status >= 200 && res.status < 300) {
       console.log("Product deleted");
-      setMyProducts((prevProducts:any) =>{
-        return prevProducts.filter((prod:any)=>{
-          return prod._id!==id
-        })
-      })
+      setMyProducts((prevProducts: any) => {
+        return prevProducts.filter((prod: any) => {
+          return prod._id !== id;
+        });
+      });
     } else {
       console.log("error");
     }
   };
+
+  const EditProductHandler = () => {
+    router.push(`/${locale}/my-products/${id}`);
+  };
+
   return (
     <div className="flex flex-row-reverse gap-3">
       <Button className={buttonClassD} onClick={deleteProductHandler}>
@@ -49,7 +56,7 @@ export default function ButtonsRenderer({ id,setMyProducts }: any) {
           height={24}
         />
       </Button>
-      <Button className={buttonClassE}>
+      <Button className={buttonClassE} onClick={EditProductHandler}>
         <Image
           src={`/client/my-products/edit.svg`}
           alt={`edit`}
