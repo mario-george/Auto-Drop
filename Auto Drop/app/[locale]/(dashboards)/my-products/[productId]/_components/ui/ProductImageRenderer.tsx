8ce3c926030a8af02c422f95b7f32a87e950eddd
mario-george <vortexspades@gmail.com>
2024-02-27 22:16:03 +0200

    Refactor ProductImageRenderer component

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductImageRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductImageRenderer.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-export default function ProductImageRenderer({product}:any) {
+export default function ProductImageRenderer({ product }: any) {
   return (
     <div>
-       <div>
+      <div className="flex flex-col">
+        <div className="flex">
           <Image
             src={product?.images[0].original}
             alt="Product Image"
@@ -13,6 +14,23 @@ export default function ProductImageRenderer({product}:any) {
             className="rounded-md mx-auto"
           />{" "}
         </div>
+        <div className="flex flex-wrap space-s-3 my-3">
+          {product?.images.map((image: any, index: number) => {
+            return (
+              index > 0 && (
+                
+                <Image
+                  src={image.original}
+                  alt="Product Image"
+                  width={900}
+                  height={1200}
+                  className="rounded-md w-[57px] h-[75px]"
+                />
+              )
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
