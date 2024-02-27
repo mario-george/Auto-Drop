747214076e4f5cc49d854ccd70a6b241ce1d002a
mario-george <vortexspades@gmail.com>
2024-02-27 12:39:37 +0200

    Refactor ProductInfoDetails component

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductInfoDetails.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductInfoDetails.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
-export default function ProductInfoDetails() {
+import React from "react";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+export default function ProductInfoDetails({
+  form,
+  productName,
+  productSku,
+  prodNameTitlePlaceholder,
+  inputClasses,
+  prodNameTitle,
+  sku,
+  setErrorMsg,
+}: any) {
   return (
     <div>
-      
+      <div className="md:flex md:justify-between md:gap-5 items-center mb-3">
+        <FormField
+          control={form.control}
+          name="prodName"
+          render={({ field }) => (
+            <FormItem className="basis-1/2">
+              <FormLabel className=" text-sm md:text-base">
+                {prodNameTitle}
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={prodNameTitlePlaceholder}
+                  {...field}
+                  id="firstName"
+                  className={`shadow-sm text-sm md:text-base ${inputClasses} `}
+                  onFocus={() => setErrorMsg(null)}
+                  value={productName}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col md:gap-2">
+          <div>{sku}</div>
+          <div>
+            <Input
+              className={`shadow-sm text-sm md:text-base ${inputClasses} `}
+              value={productSku}
+            />
+          </div>
+        </div>
+      </div>
+      <Separator />
     </div>
-  )
+  );
 }
