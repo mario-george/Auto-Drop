c9f0322e01c5150391ceb3f4eff526d1676a1aa2
mario-george <vortexspades@gmail.com>
2024-02-27 22:16:10 +0200

    Add Chakra UI Checkbox for Product Options

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductOptions.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductOptions.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Separator } from "@/components/ui/separator";
+import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
 export default function ProductOptions({ options }: any) {
   if (!options) {
     return <div>No options Found</div>;
@@ -8,7 +9,6 @@ export default function ProductOptions({ options }: any) {
   return (
     <>
       <div className="grid grid-cols-3 gap-4 p-3">
-        
         {options &&
           options.map((option: any) => {
             console.log(option.name);
@@ -17,17 +17,22 @@ export default function ProductOptions({ options }: any) {
               <>
                 {option.name.includes("Color") ? (
                   <>
-                    <span className="font-bold col-span-3">Colors</span>
+                    <span className="text-xl font-semibold col-span-3">
+                      {option.name}
+                    </span>
                     {option.values.map((el: any) => {
                       return (
                         <div key={el.id}>
-                          <Image
-                            src={el.sku_image}
-                            alt={el.name}
-                            width="250"
-                            height="500"
-                          />
-                          <p className="text-center">{el.name}</p>
+                          <Checkbox defaultChecked className="">
+                            <Image
+                              src={el.sku_image}
+                              alt={el.name}
+                              width="250"
+                              height="500"
+                              // className="my-auto"
+                            />
+                            <p className="text-center mt-auto">{el.name}</p>
+                          </Checkbox>
                         </div>
                       );
                     })}
@@ -36,18 +41,12 @@ export default function ProductOptions({ options }: any) {
                 ) : (
                   <></>
                 )}
-                   {option.name.includes("Size") ? (
+                {option.name.includes("Size") ? (
                   <>
                     <span className="font-bold col-span-3">Size</span>
                     {option.values.map((el: any) => {
                       return (
                         <div key={el.id}>
-                      {/*     <Image
-                            src={el.sku_image}
-                            alt={el.name}
-                            width="250"
-                            height="500"
-                          /> */}
                           <p className="text-center">{el.name}</p>
                         </div>
                       );
