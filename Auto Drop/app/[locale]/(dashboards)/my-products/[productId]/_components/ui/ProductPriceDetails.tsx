c9b4193cb78745b17db4c93eca85d71c50d5f389
mario-george <vortexspades@gmail.com>
2024-02-27 13:01:23 +0200

    Add offer price toggle and input field for target original price

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductPriceDetails.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductPriceDetails.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Switch } from "@chakra-ui/react";
+import { Input } from "@/components/ui/input";
 
-export default function ProductPriceDetails() {
+export default function ProductPriceDetails({
+  offerPrice,
+  addOfferPrice,target_original_price
+}: any) {
+  const [checked, setChecked] = useState(false);
   return (
     <div>
-      
+      <div className="flex space-s-3 items-center">
+        <div>{addOfferPrice}</div>
+        <Switch
+          id="email-alerts"
+          isChecked={checked}
+          onChange={() => {
+            setChecked(!checked);
+          }}
+        />
+      </div>
+      {checked && (
+        <>
+          <div>{offerPrice}</div>
+          <Input type="number" value={target_original_price} className="" />
+        </>
+      )}
     </div>
-  )
+  );
 }
