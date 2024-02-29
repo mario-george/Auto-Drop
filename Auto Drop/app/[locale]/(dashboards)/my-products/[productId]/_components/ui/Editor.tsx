d1d01576c294ba5378b4d3c3fd750a825e186e7b
mario-george <vortexspades@gmail.com>
2024-02-29 13:28:20 +0200

    Add Editor component

---
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/Editor.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function Editor({
+  onChange,
+  value,
+}: {
+  value: string | undefined;
+  onChange: (
+    value: string | undefined,
+    delta: any,
+    source: any,
+    editor: ReactQuill.UnprivilegedEditor
+  ) => void;
+}) {
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      className="bg-[#edf5f9] border-red-500 !rounded-xl shadow"
+    />
+  );
+}
