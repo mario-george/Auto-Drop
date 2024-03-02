c292f31f99c6b3f332a16e503384e8f50fbfc159
mario-george <vortexspades@gmail.com>
2024-03-02 02:35:04 +0200

    Add form fields for SEO metadata in ProductSEOInfo component

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductSEOInfo.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductSEOInfo.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function ProductSEOInfo({
   SEOTitle,
   SEODescription,
   locale,
-  metadata_description,
-  metadata_title,setMetadataDesc,setMetadataTitle
+  metadataDesc,
+  metadataTitle,
+  setMetadataDesc,
+  setMetadataTitle,
+  form,
 }: any) {
   return (
     <div>
@@ -18,10 +29,37 @@ export default function ProductSEOInfo({
           locale === "ar" ? `text-right` : `text-left`
         )}
       >
+        <FormField
+          control={form.control}
+          name="SEOTitle"
+          render={({ field }) => (
+            <FormItem className="basis-1/2">
+              <FormLabel className="shadow-sm text-sm md:text-base">
+                {SEOTitle}
+              </FormLabel>
+              <FormControl>
+                <Textarea className={`inputField `} {...field} id="SEOTitle" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <div>{SEOTitle}</div>
-        <Input className={`inputField `} value={metadata_title} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setMetadataTitle(e.target.value)}} />
+        <Textarea
+          className={`inputField `}
+          value={metadataTitle}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            setMetadataTitle(e.target.value);
+          }}
+        />
         <div>{SEODescription}</div>
-        <Input className={`inputField `} value={metadata_description} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setMetadataDesc(e.target.value)}}/>
+        <Textarea
+          className={`inputField `}
+          value={metadataDesc}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            setMetadataDesc(e.target.value);
+          }}
+        />
         <div className="flex flex-col">
           <div></div>
         </div>
