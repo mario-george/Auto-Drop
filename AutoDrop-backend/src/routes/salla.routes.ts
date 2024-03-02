c91656a14f0944a172d8cd5d15bf1cd5e046dda0
mario-george <vortexspades@gmail.com>
2024-03-02 02:33:16 +0200

    Add delete route for Salla product

--- AutoDrop-backend/src/routes/salla.routes.ts
+++ AutoDrop-backend/src/routes/salla.routes.ts
@@ -1,8 +1,14 @@
 import Authentication from "../assits/Authentication";
 import { GetAllCategories } from "../controllers/salla/categories/GetAllCategories";
+import { DeleteSallaProduct } from "../controllers/salla/products/DeleteSallaProduct";
 
 const Router = require("express").Router;
 
 const sallaRoutes = Router();
 sallaRoutes.get("/categories", [Authentication()], GetAllCategories);
+sallaRoutes.delete(
+  "/deleteProduct/:sallaProductId",
+  [Authentication()],
+  DeleteSallaProduct
+);
 export default sallaRoutes;
