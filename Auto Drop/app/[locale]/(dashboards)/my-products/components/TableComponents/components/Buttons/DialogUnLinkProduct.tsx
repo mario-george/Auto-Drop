7f7301e85cc9d148c7c96434498f25ca2324e32b
mario-george <vortexspades@gmail.com>
2024-03-04 00:10:43 +0200

    Refactor DialogUnLinkProduct component

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/DialogUnLinkProduct.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Buttons/DialogUnLinkProduct.tsx
@@ -13,30 +13,43 @@ import { Button } from "@/components/ui/button";
 import { FaPlus } from "react-icons/fa6";
 import { useRouter } from "next/navigation";
 import axiosInstance from "@/app/[locale]/(dashboards)/_components/shared/AxiosInstance";
-import Image from 'next/image'
-export default function SubmitProducts({ sallaProductId,setLoadProducts }: any) {
+import Image from "next/image";
+import useLoader from "@/components/loader/useLoader";
+
+export default function SubmitProducts({
+  sallaProductId,
+  setLoadProducts,
+}: any) {
   const router = useRouter();
+  const { setLoading, LoaderComponent } = useLoader();
+
   let submitHandler = async () => {
-    const resp = await axiosInstance.delete(`/salla/deleteProduct/${sallaProductId}`);
+    setLoading(true);
+    const resp = await axiosInstance.delete(
+      `/salla/deleteProduct/${sallaProductId}`
+    );
     console.log(resp.data);
-    if(resp.data.status){
-      setLoadProducts((prev:boolean)=>!prev)
+    if (resp.data.status) {
+      setLoadProducts((prev: boolean) => !prev);
     }
+    setLoading(false);
   };
   let buttonClassL = `rounded-full
     bg-red-500
-   px-2 py-2`;
+    px-2 py-2 w-[2rem] h-[2rem] tab:w-[3rem] tab:h-[3rem] hover:cursor-pointer  hover:bg-red-500/90 `;
   return (
     <>
       <Dialog submitHandler={submitHandler}>
-      <Button className={buttonClassL} >
-        <Image
-          src={`/client/my-products/link.svg`}
-          alt={`link`}
-          width={24}
-          height={24}
-        />
-      </Button>
+        {LoaderComponent}
+        <div className={buttonClassL}>
+          <Image
+            src={`/client/my-products/link.svg`}
+            alt={`link`}
+            width={24}
+            height={24}
+            className="mx-auto my-auto  mt-[15%] tab:mt-[22.5%]"
+          />
+        </div>
       </Dialog>
     </>
   );
