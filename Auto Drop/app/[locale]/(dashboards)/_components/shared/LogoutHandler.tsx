d7ba879c392f6597b2372daa41a1915f8d79ab43
mario-george <vortexspades@gmail.com>
2024-03-03 15:06:24 +0200

    Refactor LogoutHandler component to include isNavOpen prop

--- Auto Drop/app/[locale]/(dashboards)/_components/shared/LogoutHandler.tsx
+++ Auto Drop/app/[locale]/(dashboards)/_components/shared/LogoutHandler.tsx
@@ -5,25 +5,25 @@ import { userActions } from "@/store/user-slice";
 import { motion } from "framer-motion";
 import LogoutSVG from "@/components/icons/ClientSVGs/LogoutSVG";
 
-export default function LogoutHandler({logoutMsg,marginDirection}:{logoutMsg:string,marginDirection:string}) {
+export default function LogoutHandler({logoutMsg,marginDirection,isNavOpen}:{logoutMsg:string,marginDirection:string,isNavOpen?:boolean}) {
   // dir={locale === "ar" ? "rtl" : "ltr"}
   return (
     <>
       <div className="">
       
-        <motion.li className="cursor-pointer group icon-path flex items-center py-2  px-4 hover:bg-[#F0F3F400] text-[#253439] hover:text-black hover:bg-[#f0f3f4] rounded-lg hover:shadow-md"
+        <motion.li className={`cursor-pointer group icon-path flex items-center py-2  px-4 hover:bg-[#F0F3F400] text-[#253439] hover:text-black hover:bg-[#f0f3f4] rounded-lg hover:shadow-md ${isNavOpen &&`!pr-12 mr-24 `}`}
           onClick={() => store.dispatch(userActions.logout())}
           >
             <div className="flex items-center">
               <LogoutSVG />
-              <motion.span
+              {!isNavOpen &&          <motion.span
                 className={`${marginDirection} whitespace-nowrap`}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
               >
                 {logoutMsg}
-              </motion.span>
-            </div>
+              </motion.span>}
+                     </div>
           </motion.li>
 
       </div>
