2bf4d5cd282b0a8020833c886d57b5f8f17c33f3
mario-george <vortexspades@gmail.com>
2024-03-07 20:01:58 +0200

    Add theme provider and toaster to Chakra UI providers

--- Auto Drop/components/chakra-ui/providers.tsx
+++ Auto Drop/components/chakra-ui/providers.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { Toaster } from "@/components/ui/toaster"
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster/>
+        {children}
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
