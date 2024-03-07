5840f9f28397b9935f0cc884b180d9141f141f8c
mario-george <vortexspades@gmail.com>
2024-03-07 20:01:37 +0200

    Refactor ProductShipping component UI

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductShipping.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductShipping.tsx
@@ -22,29 +22,29 @@ export default function ProductShipping({
                   return (
                     <Radio value={index.toString()} key={index}>
                       <div
-                        style={{ border: "2px solid #d1c2c2" }}
-                        className={` flex  flex-col border-gray-500  rounded-lg tab:px-3 tab:py-5 ${
+                        // style={{ border: "2px solid #d1c2c2" }}
+                        className={` flex  flex-col border-gray-500  rounded-lg tab:px-3 tab:py-5 !text-xs ${
                           value == index.toString()
                             ? `shippingCardActive`
                             : `shippingCard`
                         } shippingCard gap-y-3`}
                         key={"option-" + index}
                       >
-                        <div className="flex w-full justify-between items-center px-2 pt-2 ">
+                        <div className="flex w-full justify-between items-center px-2 pt-2  ">
                           <div className="flex items-center gap-x-2">
-                            <p className={`  ${value == index.toString() ? `text-white`:`text-[#263238]` } text-white whitespace-nowrap`}>{nameOfShippingComp} </p>
-                            <p className={`text-sm  ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }  whitespace-nowrap `}>
+                            <p className={`text-xs tab:text-sm font-bold  ${value == index.toString() ? `text-white`:`text-[#263238]` }  whitespace-nowrap`}>{nameOfShippingComp} </p>
+                            <p className={`text-xs tab:text-sm ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }  whitespace-nowrap `}>
                               {" "}
 
                               {option.shipping_method || option.service_name}
                             </p>
-                            <span className={`text-xs${value == index.toString() ? `text-white`:`text-[#263238]` } tab:text-lg whitespace-nowrap`}>{durationToDeliver}</span>
-                            <span className={` ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }`}>
+                            <span className={`text-xs ${value == index.toString() ? `text-white`:`text-[#263238]` } text-xs tab:text-sm font-bold whitespace-nowrap`}>{durationToDeliver}</span>
+                            <span className={` text-xs tab:text-sm ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }`}>
                               {option.estimated_delivery_time}
                             </span>
                             {/*    <p className="text-xs px-2">
                         </p> */}
-                            <p className={`text-xs tab:text-lg   tab:pr-2 ${value == index.toString() ? `text-[#F4B1AC]`:`text-[#C1121F]` }`}>
+                            <p className={`text-xs tab:text-lg font-bold  tab:pr-2 ${value == index.toString() ? `text-[#F4B1AC]`:`text-[#C1121F]` }`}>
                               {CurrencyFormatter(
                                 option.freight.amount ||
                                   option.freight.cent / 100
