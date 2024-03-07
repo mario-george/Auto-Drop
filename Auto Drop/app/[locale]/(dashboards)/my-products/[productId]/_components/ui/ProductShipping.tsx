bfaefe73ef7a0053d88d969ef733d3b3612b4586
mario-george <vortexspades@gmail.com>
2024-03-07 20:55:02 +0200

    Refactor ProductShipping component UI

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductShipping.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductShipping.tsx
@@ -13,37 +13,33 @@ export default function ProductShipping({
   return (
     <>
       {shipping?.length ? (
-        <div className="space-y-4 min-w-full ">
+        <div className="space-y-4 min-w-full  ">
           <p className="text-lg font-semibold text-content">{shippingText}</p>
-          <div className="min-w-full grid grid-cols-1 items-center justify-around gap-y-5 px-2 py-4">
+          <div className="min-w-full  grid grid-cols-1 items-center justify-around gap-y-5 py-4">
             <RadioGroup defaultValue="1" onChange={setValue} value={value}>
-              <Stack direction="row">
                 {shipping?.map((option: any, index: number) => {
                   return (
                     <Radio value={index.toString()} key={index}>
                       <div
-                        // style={{ border: "2px solid #d1c2c2" }}
-                        className={` flex  flex-col border-gray-500  rounded-lg tab:px-3 tab:py-5 !text-xs ${
+                        className={` flex  border-gray-500  rounded-lg tab:px-3 tab:py-5 !text-xs ${
                           value == index.toString()
                             ? `shippingCardActive`
                             : `shippingCard`
                         } shippingCard gap-y-3`}
                         key={"option-" + index}
                       >
-                        <div className="flex w-full justify-between items-center px-2 pt-2  ">
-                          <div className="flex items-center gap-x-2">
-                            <p className={`text-xs tab:text-sm font-bold  ${value == index.toString() ? `text-white`:`text-[#263238]` }  whitespace-nowrap`}>{nameOfShippingComp} </p>
-                            <p className={`text-xs tab:text-sm ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }  whitespace-nowrap `}>
+                          <div className="flex flex-col space-y-3 tab:space-y-0 tab:flex-row items-center space-s-2 px-4 py-2 ">
+                            <p className={`text-xs tab:text-sm font-bold  ${value == index.toString() ? `text-white`:`text-[#263238]` }  `}>{nameOfShippingComp} </p>
+                            <p className={`text-xs tab:text-sm text-center ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }   `}>
                               {" "}
 
                               {option.shipping_method || option.service_name}
                             </p>
-                            <span className={`text-xs ${value == index.toString() ? `text-white`:`text-[#263238]` } text-xs tab:text-sm font-bold whitespace-nowrap`}>{durationToDeliver}</span>
+                            <span className={`text-xs ${value == index.toString() ? `text-white`:`text-[#263238]` } text-xs tab:text-sm font-bold text-center `}>{durationToDeliver}</span>
                             <span className={` text-xs tab:text-sm ${value == index.toString() ? `text-[#CCE0DB]`:`text-[#263238]` }`}>
                               {option.estimated_delivery_time}
                             </span>
-                            {/*    <p className="text-xs px-2">
-                        </p> */}
+                    
                             <p className={`text-xs tab:text-lg font-bold  tab:pr-2 ${value == index.toString() ? `text-[#F4B1AC]`:`text-[#C1121F]` }`}>
                               {CurrencyFormatter(
                                 option.freight.amount ||
@@ -51,12 +47,10 @@ export default function ProductShipping({
                               )}
                             </p>
                           </div>
-                        </div>
                       </div>
                     </Radio>
                   );
                 })}
-              </Stack>
             </RadioGroup>
           </div>
         </div>
