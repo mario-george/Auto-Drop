7e468bf3a078c6a45e1ca9e9775647d269d4adc5
mario-george <vortexspades@gmail.com>
2024-03-07 20:01:09 +0200

    Refactor Header component to include ProfitComponent and LoaderComponent

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
@@ -1,14 +1,37 @@
+"use client";
+import useProfitTypeHandler from "./useProfitTypeHandler";
 export default function Header({
+  upProducts,
+  val,
+  percentage,
+  profitType,
+  number,
   title,
   className,
 }: {
+  upProducts: string;
+  val: string;
+  number: string;
+  percentage: string;
+  profitType: string;
   title: string;
   className?: string;
 }) {
+  const { ProfitComponent,LoaderComponent } = useProfitTypeHandler({
+    upProducts,
+    val,
+    percentage,
+    profitType,
+    number,
+  });
   return (
     <>
-      <div className={`flex tab:text-[30px] text-[#253439] ${className}`}>
-        {title}
+    {LoaderComponent}
+      <div className="flex justify-between max-w-[90%] px-3 my-3 items-center">
+        <div className={`flex tab:text-[30px] text-[#253439] ${className}`}>
+          {title}
+        </div>{" "}
+        <div>{ProfitComponent}</div>
       </div>
     </>
   );
