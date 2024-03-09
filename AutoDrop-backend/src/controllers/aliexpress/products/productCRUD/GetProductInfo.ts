7a21a82ed464e4f9ef5d2691127df1ae8a440a15
mario-george <vortexspades@gmail.com>
2024-03-09 13:57:49 +0200

    Refactor GetProductInfo controller to use catchAsync

--- AutoDrop-backend/src/controllers/aliexpress/products/productCRUD/GetProductInfo.ts
+++ AutoDrop-backend/src/controllers/aliexpress/products/productCRUD/GetProductInfo.ts
@@ -1,22 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { Product } from "../../../../models/product.model";
+import catchAsync from "../../../../utils/catchAsync";
 
-export default async function GetProductInfo(
-  req: Request & any,
-  res: Response,
-  next: NextFunction
-) {
-  let { productId } = req.params;
-  const product = await Product.findById(productId);
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
+const GetProductInfo = catchAsync(
+  async (req: Request & any, res: Response, next: NextFunction) => {
+    let { productId } = req.params;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    console.log(product.merchant);
+    console.log(req.user._id);
+    if (product.merchant !== ((req.user?._id).toString() as string)) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to view this product" });
+    }
+    return res.status(200).json({ product });
   }
-  console.log(product.merchant);
-  console.log(req.user._id);
-  if (product.merchant !== ((req.user?._id).toString() as string)) {
-    return res
-      .status(403)
-      .json({ message: "You are not authorized to view this product" });
-  }
-  return res.status(200).json({product});
-}
+);
+export default GetProductInfo;
