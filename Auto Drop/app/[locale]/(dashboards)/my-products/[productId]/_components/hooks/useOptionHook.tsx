d337f11e0e25e082200a2915d8933fa38d067321
mario-george <vortexspades@gmail.com>
2024-03-09 13:57:57 +0200

    Refactor useOptionHook to use product options

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/hooks/useOptionHook.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/hooks/useOptionHook.tsx
@@ -5,25 +5,30 @@ interface Option {
   checkboxes: boolean[];
 }
 
-export default function useOptionHook({ options }: any) {
+export default function useOptionHook({ product }: any) {
   const [optionsSelected, setOptionsSelected] = useState<Option[]>([]);
   const optionCheckHandler = (OptionIndex: number, valueIndex: number) => {
+    console.log("OptionIndex",OptionIndex)
+    console.log("valueIndex",valueIndex)
     setOptionsSelected((prev: any) => {
       const updatedOptions = [...prev];
+      console.log("updatedOptions[OptionIndex].checkboxes[valueIndex]",updatedOptions[OptionIndex].checkboxes[valueIndex])
+      console.log("!updatedOptions[OptionIndex].checkboxes[valueIndex]",!updatedOptions[OptionIndex].checkboxes[valueIndex])
       updatedOptions[OptionIndex].checkboxes[valueIndex] =
         !updatedOptions[OptionIndex].checkboxes[valueIndex];
+        console.log("updatedOptions",updatedOptions)
       return updatedOptions;
     });
   };
   useEffect(() => {
-    let newOpt = options?.map((option: any) => {
+    let newOpt = product?.options?.map((option: any) => {
       let { name, values } = option;
       let checkboxes = Array(values.length).fill(true);
-      return { name, checkboxes };
+      return { name, checkboxes ,values};
     });
     setOptionsSelected((prev: any) => {
       return newOpt;
     });
-  }, [options]);
+  }, [product]);
   return { optionsSelected, setOptionsSelected, optionCheckHandler };
 }
