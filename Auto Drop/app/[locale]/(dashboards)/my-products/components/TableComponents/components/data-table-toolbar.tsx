fe5d2fda5a85d20da7be585814af9e537f934ac5
mario-george <vortexspades@gmail.com>
2024-03-10 01:22:31 +0200

    Update DataTableToolbar component styling

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/data-table-toolbar.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/data-table-toolbar.tsx
@@ -75,7 +75,7 @@ export function DataTableToolbar<TData>({
             onChange={(event) =>
               table.getColumn("prodName")?.setFilterValue(event.target.value)
             }
-            className="placeholder:text-xs w-[150px] lg:w-[350px] shadow-md rounded-lg !placeholder-opacity-1  placeholder:text-[#b0b0b0] dark:bg-white"
+            className="placeholder:text-xs w-[150px] lg:w-[350px] shadow-md rounded-lg !placeholder-opacity-1  placeholder:text-[#b0b0b0] dark:bg-white dark:text-black"
           />
           <div
             className={cn(
