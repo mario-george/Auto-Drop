793f513a9e488fa55663ff5b13840d325661aa94
mario-george <vortexspades@gmail.com>
2024-03-13 14:43:43 +0200

    Add useAddCard hook to Wallet component

--- Auto Drop/app/[locale]/(dashboards)/wallet/_components/Wallet.tsx
+++ Auto Drop/app/[locale]/(dashboards)/wallet/_components/Wallet.tsx
@@ -13,6 +13,7 @@ import { AnimatePresence } from "framer-motion";
 
 import WalletUserCard from "./WalletUserCard";
 import HeaderText from "./HeaderText";
+import useAddCard from "./hooks/useAddCard";
 
 interface WalletProps {
   locale?: string;
@@ -30,14 +31,9 @@ export default function Settings(props: WalletProps) {
     myCards,
     addCard,
     chargeWallet,
-    /*     settings,
-    changePassword,
-    changeAccountDetails,
-    newPassword,
-    currentPassword,
-    confirmPassword,
-    saveChanges, */
+    
   } = props;
+  const {value,AddCardComponent} = useAddCard({chargeWallet})
   const isAr = locale === "ar";
   let WalletUserCardProps = {
     name: user.name,
@@ -48,53 +44,26 @@ export default function Settings(props: WalletProps) {
       <HeaderText isAr={isAr} title={wallet} />
 
       <div
-        className={`relative bg-white  text-[#253439]   rounded-lg  !px-0 mt-4 overflow-hidden ${
+        className={`relative  text-[#253439]   rounded-lg  !px-0 mt-4 overflow-hidden ${
           isAr ? `ml-3 tab:ml-3 tab:mr-3` : `mr-3 tab:mr-3 tab:ml-3`
         }  `}
         dir={locale === "en" ? "ltr" : "rtl"}
       >
-        {/*   <div className="absolute top-12 right-0 left-0">
-          <svg
-            width="1607"
-            height="1"
-            viewBox="0 0 1607 1"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line
-              x1="1607"
-              y1="0.5"
-              x2="-4.37115e-08"
-              y2="0.49986"
-              stroke="#707070"
-            />
-          </svg>
-        </div>
- */}
+   <div className="bg-white dark:bg-[#2e464f]  rounded-lg p-4 my-4">
+
 
         <WalletUserCard {...WalletUserCardProps} />
-        <div className={` tab:px-6 py-2 my-12 tab:mx-3`}>
-          {/*   <div className="flex space-s-6 tab:space-s-0 flex-wrap tab:max-w-[60%] lap:max-w-[50%] tab:mb-4">
-            <Button
-              className="w-full ms:max-w-[60%] ms:mx-auto tab:mx-auto tab:max-w-[150px] bg-[#f0f3f4] rounded-lg text-[#253439] my-2 sm:my-0 hover:bg-[#f0f3f4]"
-              onClick={() => {
-                // setCurrWindow("ChangePassword");
-              }}
-            >
-              {"changePassword"}
-            </Button>
-            <Button
-              className="w-full ms:max-w-[60%] ms:!mx-auto tab:!mx-auto tab:max-w-[150px] bg-[#253439] rounded-lg my-2 sm:my-0 hover:bg-[#253439]"
-              onClick={() => {
-                // setCurrWindow("AccountInfo");
-              }}
-            >
-              {"changeAccountDetails"}
-            </Button>
-          </div> */}
-        </div>
-      </div>
+   </div>
+
+ {/*        <div className={` tab:px-6 py-2 my-12 tab:mx-3`}>
+      
+        </div> */}
       <HeaderText isAr={isAr} title={myCards} />
+      <div className="bg-white dark:bg-[#2e464f] rounded-lg p-4 my-4">
+
+{AddCardComponent}
+      </div>
+   </div>
       <HeaderText isAr={isAr} title={addCard} />
     </MotionWrapper>
   );
