5c556dd0ff8620c60d1be5c56a2b022ef7da9b34
mario-george <vortexspades@gmail.com>
2024-03-13 14:43:22 +0200

    Refactor WalletUserCard component styles

--- Auto Drop/app/[locale]/(dashboards)/wallet/_components/WalletUserCard.tsx
+++ Auto Drop/app/[locale]/(dashboards)/wallet/_components/WalletUserCard.tsx
@@ -9,17 +9,17 @@ export default function WalletUserCard({
   chargeWallet,
 }: any) {
   return (
-    <div className="bg-white rounded-lg text-[#253439]">
-      <div className="max-w-[40%] flex flex-col justify-center border-2 border-gray-300 rounded-lg p-4 space-y-4">
+    <div className="bg-white rounded-lg text-[#253439] mx-8 py-10 dark:bg-[#2e464f]">
+      <div className="max-w-[40%] flex flex-col justify-center border-2 border-gray-300 rounded-lg p-4 space-y-4 overflow-hidden dark:bg-white">
         <div>{name}</div>
         <div className="text-2xl ">{CurrencyFormatter(balance)}</div>
-        <div className="bg-[#ECEDEE] rounded-xl flex-1 -mx-4 py-4 px-2 ">
+        <div className="bg-[#ECEDEE] rounded-xl flex-1 -mx-4 py-4 px-2 transform translate-y-4 ">
           {number}
         </div>
       </div>{" "}
-      <div className="flex space-s-4 mt-4">
-        <Input className="max-w-[20%]" />
-        <Button className="bg-[#B29E84] hover:bg-[#B29E84]/90">
+      <div className="flex space-s-4 mt-8">
+        <Input className="max-w-[30%]" />
+        <Button className="bg-[#B29E84] hover:bg-[#B29E84]/90 ">
           {" "}
           {chargeWallet}
         </Button>
