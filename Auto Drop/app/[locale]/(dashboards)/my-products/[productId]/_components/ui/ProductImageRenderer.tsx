957892d0dc1554ac947d6a57507dd1392e2a3db6
mario-george <vortexspades@gmail.com>
2024-03-15 13:22:41 +0200

    Refactor ProductImageRenderer component

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductImageRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/ProductImageRenderer.tsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { Radio, RadioGroup } from '@chakra-ui/react';
+import { Radio, RadioGroup } from "@chakra-ui/react";
 
 interface ProductImage {
-  alt:string
-  default:boolean
-  original:string
-  thumbnail:string
+  alt: string;
+  default: boolean;
+  original: string;
+  thumbnail: string;
 }
 
-export default function ProductImageRenderer({ setProductImages ,productImages}: any) {
+export default function ProductImageRenderer({
+  setProductImages,
+  productImages,
+}: any) {
   // const [value,setValue]=useState(product?.images[0].original)
 
+  let defaultImageIndex = 0;
+  if (productImages && productImages?.length > 0) {
+    defaultImageIndex = productImages.findIndex((image: ProductImage) => {
+      return image.default;
+    });
+  }
+  const handleImageDefaultChange = (newIndexString: string) => {
+    let newIndex = Number(newIndexString);
+    setProductImages((prevImages: ProductImage[]) => {
+      let tempImages = [...prevImages];
+      let firstImage = tempImages[newIndex];
+      tempImages = tempImages
+        .map((image: ProductImage) => {
+          return { ...image, default: false };
+        })
+        .filter((image: ProductImage, index: number) => {
+          return index !== newIndex;
+        });
 
-  let defaultImageIndex = 0
-if(productImages&&productImages?.length>0){
-defaultImageIndex = productImages.findIndex((image:ProductImage)=>{
-return image.default
-})
-}
-const handleImageDefaultChange = (newIndexString:string)=>{
-  let newIndex = Number(newIndexString)
-  setProductImages((prevImages:ProductImage[])=>{
-let tempImages = [...prevImages]
-let firstImage =tempImages[newIndex]
-tempImages = tempImages.map((image:ProductImage)=>{
-  return {...image,default:false}
-}).filter((image:ProductImage,index:number)=>{return index!==newIndex})
-
-firstImage.default =true
-return [firstImage,...tempImages]
-  })
-}
+      firstImage.default = true;
+      return [firstImage, ...tempImages];
+    });
+  };
 
   // console.log("product.images", product.images);
   console.log("productImages", productImages);
@@ -47,26 +53,25 @@ return [firstImage,...tempImages]
             className="rounded-md mx-auto"
           />{" "}
         </div>
-        <RadioGroup value={defaultImageIndex.toString()} onChange={handleImageDefaultChange}>
-        <div className="grid grid-cols-3 tab:grid-cols-5 justify-center gap-3 my-3">
-          {productImages.map((image: any, index: number) => {
-            return (
-               (
-                <Radio  value={index.toString()} key={index.toString()}>
-                <Image
-                  src={image.original}
-                  alt="Product Image"
-                  width={900}
-                  height={1200}
-                  className="rounded-md w-[57px] h-[75px]"
-                />
-
-</Radio>
-
-              )
-            );
-          })}
-        </div>
+        <RadioGroup
+          value={defaultImageIndex.toString()}
+          onChange={handleImageDefaultChange}
+        >
+          <div className="grid grid-cols-3 tab:grid-cols-5 justify-center gap-3 my-3">
+            {productImages.map((image: any, index: number) => {
+              return (
+                <Radio value={index.toString()} key={index.toString()}>
+                  <Image
+                    src={image.original}
+                    alt="Product Image"
+                    width={900}
+                    height={1200}
+                    className="rounded-md w-[57px] h-[75px]"
+                  />
+                </Radio>
+              );
+            })}
+          </div>
         </RadioGroup>
       </div>
     </div>
