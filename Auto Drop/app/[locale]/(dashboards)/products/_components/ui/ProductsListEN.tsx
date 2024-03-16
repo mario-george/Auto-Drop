15743c9c0e6999fa4f94ff717f6358268db099a5
mario-george <vortexspades@gmail.com>
2024-03-16 18:11:01 +0200

    refactor products list en

--- Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListEN.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListEN.tsx
@@ -19,13 +19,8 @@ import CartSVG from "../../../../../../public/client/products/CartSVG";
 export default function ProductsListEN({
   products,
   productsShippingInfo,
-  toggleShoppingCartActivated,
   handleCheckChange,
   locale,
-  shippingInfoPending,
-  commissionV,
-  setCommissionV,
-  addCommissionHandler,
   showShippingForProduct,
   showShippingHandler,
 }: any) {
@@ -63,10 +58,10 @@ export default function ProductsListEN({
                   <>
                     <MotionWrapperExit locale="en">
                       <div className="text-[#253439] tab:text-sm  " dir="ltr">
-                        <div className="mx-auto bg-[#f0f3f4] text-center pt-16 p-6  ">
+                        <div className="shippingMethodsTitle ">
                           Information and shipping methods for the product
                         </div>
-                        <ScrollArea className="h-[18rem]">
+                        <ScrollArea className="scrollAreaShipping">
                           {productsShippingInfo[i].map(
                             (shipping: any, ind: number) => {
                               console.log(
@@ -108,15 +103,7 @@ export default function ProductsListEN({
                                         {shipping.duration}
                                       </span>
                                     </div>
-                                    {/*   <div className="flex space-s-3">
-                                        {" "}
-                                        <span>Profit After Discount:</span>{" "}
-                                        <span className="text-[#008767]">
-                                          {CurrencyFormatterShippingInfo(
-                                            shipping.profitAfterDiscount
-                                          )}
-                                        </span>{" "}
-                                      </div> */}
+                                 
                                     <div className="flex space-s-3 text-[#C1121F]">
                                       {" "}
                                       <span>Price:</span>{" "}
@@ -150,12 +137,11 @@ export default function ProductsListEN({
                         width={300}
                         alt="aliexpressProduct"
 
-                        // objectFit="contain" // or "contain"
 
                       />
                     </div>
 
-                    <div className="productsCard mt-auto">
+                    <div className="productsCard">
                       <div
                         className=" flex items-center justify-between"
                         dir="ltr"
@@ -167,13 +153,7 @@ export default function ProductsListEN({
                           ...
                         </div>
                         <div>
-                          {/*     <Image
-                            src={"/client/products/cart.svg"}
-                            alt={`cart`}
-                            width={24}
-                            height={24}
-                            className="fill-white text-white"
-                          /> */}
+                        
 
                           <CartSVG />
                         </div>
@@ -226,25 +206,20 @@ export default function ProductsListEN({
                 )}
                 <div
                   className={cn(
-                    "absolute top-[1rem] ",
+                    "shippingCartIcon ",
                     locale == "ar" ? `right-[1rem]` : `left-[1rem]`
                   )}
                 >
                   <div
                     className="overflow-hidden"
                     onClick={() => {
-                      /*   if (
-                        product.checked &&
-                        (product.vendor_commission || product.vendor_commission === 0)
-                      ) {
-                        toggleShoppingCartActivated(i);
-                      } */
+                   
                       showShippingHandler(i);
                     }}
                   >
                     <Image
                       src={`/client/products/shoppingCart.svg`}
-                      className=" rounded-full cursor-pointer "
+                      className="shippingCartIconImage"
                       height={45}
                       width={45}
                       alt="shippingCart"
@@ -270,123 +245,7 @@ export default function ProductsListEN({
                     />
                   </div>
                 )}
-                {/*     {!shippingInfoActive && (
-                  <div className="">
-                    <Image
-                      src={
-                        product.product_small_image_urls.productSmallImageUrl[0]
-                      }
-                      className="p-0 w-full min-h-[67.5%] mb-auto "
-                      height={300}
-                      width={300}
-                      alt="aliexpressProduct"
-                    />
-                  </div>
-                )} */}
-                {/*  
-               {
-                product.checked && !shippingInfoActive ? (
-                  <>
-                    <div className="space-y-3 flex flex-col pt-7">
-                      <span className="mx-auto">
-                        Please Enter your commision
-                      </span>
-                      <div className="flex space-x-3 items-center justify-between px-3 pb-2">
-                        <Button
-                          className="bg-blue-500 text-white hover:bg-blue-600 rounded-full "
-                          onClick={() => {
-                            addCommissionHandler(i, product, commissionV[i]);
-                          }}
-                          disabled={shippingInfoPending}
-                        >
-                          <BiSend />
-                        </Button>
-                        <div className="relative mt-auto">
-                          <Input
-                            type="number"
-                            className="pr-6"
-                            value={commissionV[i]}
-                            onChange={(e: any) => {
-                              const newValues = [...commissionV];
-                              newValues[i] = e.target.value;
-                              setCommissionV(newValues);
-                            }}
-                          />
-                          <span className="absolute inset-y-0 right-0 pr-2 flex items-center text-gray-500">
-                            %
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                ) :  shippingInfoActive ? (
-                  <></>
-                ) : (
-                  <>
-                    <div className="p-3 flex flex-col  gap-y-3">
-                      <div
-                        className=" flex items-center justify-between"
-                        dir="ltr"
-                      >
-                        <div className={` text-[#253439] text-xs`}>
-                          {product.product_title.substring(0, 35)}
-                          ...
-                        </div>
-                        <div>
-                          <Image
-                            src={"/client/products/cart.svg"}
-                            alt={`cart`}
-                            width={24}
-                            height={24}
-                          />
-                        </div>
-                      </div>
-
-                      <div
-                        className={`flex justify-between items-center w-full `}
-                      >
-                        <div className={`flex gap-x-2 items-center w-11/12 `}>
-                          <span className="text-sm text-[#253439]">
-                            {CurrencyFormatter(product.target_sale_price)}
-                          </span>
-                          {product.target_original_price !==
-                          product.target_sale_price ? (
-                            <span className="text-xs text-[#d64d57] line-through">
-                              {CurrencyFormatter(product.target_original_price)}
-                            </span>
-                          ) : (
-                            ""
-                          )} 
-                          */}
-                {/*   </div>
-                      </div>
-                      <div
-                        className={`flex justify-between items-center `}
-                        dir="ltr"
-                      >
-                        <div className="flex flex-1 z-30">
-                          {product.evaluate_rate
-                            ? renderRatingStars(
-                                product.evaluate_rate.split("%")[0]
-                              )
-                            : renderRatingStars(90)}
-                        </div>
-                        <a
-                          href={product.product_detail_url}
-                          target="_blank"
-                          className=" "
-                        >
-                          <Image
-                            src={`/client/products/aliexpressCard.svg`}
-                            width={66}
-                            height={21}
-                            alt="aliexpressCard"
-                          />
-                        </a>
-                      </div>
-                    </div>
-                  </>
-                )} */}
+           
               </Card>
             );
           })}
