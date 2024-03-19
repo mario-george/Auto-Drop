55f1e151ef00875789e2cde38c5303a56c98d8f8
mario-george <vortexspades@gmail.com>
2024-03-19 18:59:21 +0200

    Add file upload functionality to getRandomProductsImage route

--- AutoDrop-backend/src/routes/search.routes.ts
+++ AutoDrop-backend/src/routes/search.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { GetRecommendedProductsByCategory, GetRecommendedProductsByImage, GetRecommendedProductsByURL } from "../controllers/aliexpress/products/Category/SeachAliexpressProducts";
+import multer from "multer";
+const upload = multer();
 
 const searchRoutes = Router();
 searchRoutes.post("/getRandomProducts/",GetRecommendedProductsByURL)
-searchRoutes.post("/getRandomProductsImage/",GetRecommendedProductsByImage)
+searchRoutes.post("/getRandomProductsImage/", upload.single('file'), GetRecommendedProductsByImage);
 searchRoutes.post("/getRandomProductsCategory/",GetRecommendedProductsByCategory)
 
 
