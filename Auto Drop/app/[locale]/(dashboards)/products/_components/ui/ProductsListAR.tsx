4448078cd0ca2cc89a736f425c55f9843e1c9e93
mario-george <vortexspades@gmail.com>
2024-03-19 20:30:07 +0200

    Fix product image URL in ProductsListAR component

--- Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListAR.tsx
+++ Auto Drop/app/[locale]/(dashboards)/products/_components/ui/ProductsListAR.tsx
@@ -118,7 +118,7 @@ export default function ProductsListAR({
                       <Image
                         src={
                           product.product_small_image_urls
-                            .productSmallImageUrl[0]
+                            .productSmallImageUrl[0]||  product.product_main_image_url
                         }
                         className="p-0 w-full min-h-[67.5%] mb-auto "
                         height={300}
