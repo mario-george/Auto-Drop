7d9bb2da58fe11272b64d157089e8e700e6f2a19
mario-george <vortexspades@gmail.com>
2024-03-19 21:49:23 +0200

    Add errorButtonRef to useErrorToast

--- Auto Drop/components/chakra-ui/useErrorToast.tsx
+++ Auto Drop/components/chakra-ui/useErrorToast.tsx
@@ -1,12 +1,13 @@
 import { Button, useToast } from '@chakra-ui/react'
 interface ErrorToastProps {
     description?:string
-    title?:string
+    title?:string;
+    errorButtonRef?:React.RefObject<HTMLButtonElement>
 }
-export const useErrorToast = ({title,description}:ErrorToastProps) => {
+export const useErrorToast = ({title,description,errorButtonRef}:ErrorToastProps) => {
     const toast = useToast()
 
-let ErrorComponent =     <Button
+let ErrorComponent =   <div className="hidden">   <Button
 onClick={() =>
   toast({
     title,
@@ -17,8 +18,10 @@ onClick={() =>
     position: 'bottom-right',
   })
 }
+ref={errorButtonRef}
+className="!hidden"
 >
 Show Toast
-</Button>
+</Button></div>
 return {ErrorComponent}
 } 
