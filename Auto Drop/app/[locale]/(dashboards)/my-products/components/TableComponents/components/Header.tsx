2c745c241f65e1c34aba90545b33a36e36430138
mario-george <vortexspades@gmail.com>
2024-03-21 00:45:36 +0200

    Add success toast component to Header component

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
@@ -2,6 +2,7 @@
 import { useErrorToast } from "@/components/chakra-ui/useErrorToast";
 import useProfitTypeHandler from "./useProfitTypeHandler";
 import { useRef } from "react";
+import { useSuccessToast } from "@/components/chakra-ui/useSuccessToast";
 export default function Header({
   upProducts,
   val,
@@ -22,15 +23,18 @@ export default function Header({
   const errorButtonRefNoSelection = useRef<HTMLButtonElement>(null);
   const errorButtonRefNoToken = useRef<HTMLButtonElement>(null);
   const errorButtonRefSubmitError = useRef<HTMLButtonElement>(null);
+  const successButtonRef = useRef<HTMLButtonElement>(null);
+  
   const {ErrorComponent:ErrorComponentNoSelection}  = useErrorToast({title:"Error",description:"No product was selected please try again.",errorButtonRef:errorButtonRefNoSelection})
   const {ErrorComponent:ErrorComponentNoToken}  = useErrorToast({title:"Error",description:"Please link your account with salla and try again.",errorButtonRef:errorButtonRefNoToken})
   const {ErrorComponent:ErrorComponentSubmitError}  = useErrorToast({title:"Error",description:"Error while linking product.",errorButtonRef:errorButtonRefSubmitError})
+  const {SuccessComponent:SuccessComponent}  = useSuccessToast({title:"Success",description:"Products has been linked successfully.",successButtonRef:successButtonRef})
   const { ProfitComponent, LoaderComponent } = useProfitTypeHandler({
     upProducts,
     val,
     percentage,
     profitType,
-    number,errorButtonRefNoSelection,errorButtonRefNoToken,errorButtonRefSubmitError
+    number,errorButtonRefNoSelection,errorButtonRefNoToken,errorButtonRefSubmitError,successButtonRef
   });
   return (
     <>
@@ -39,6 +43,7 @@ export default function Header({
       {ErrorComponentNoSelection}
       {ErrorComponentNoToken}
       {ErrorComponentSubmitError}
+      {SuccessComponent}
       <div className="flex flex-col tab:flex-row justify-between tab:max-w-[90%] px-3 my-3 items-center">
         <div className={`flex tab:text-[30px] text-[#253439] ${className}`}>
           {title}
