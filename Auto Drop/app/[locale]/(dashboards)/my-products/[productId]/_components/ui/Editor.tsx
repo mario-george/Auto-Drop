94e6f87cc6999262bef14b51fa6715361774184f
mario-george <vortexspades@gmail.com>
2024-03-20 15:31:55 +0200

    Refactor Editor component to use forwardRef

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/Editor.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/Editor.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
-export default function Editor({
-  onChange,
-  value,
-}: {
+interface EditorProps {
   value: string | undefined;
   onChange: (
     value: string | undefined,
@@ -13,7 +9,12 @@ export default function Editor({
     source: any,
     editor: ReactQuill.UnprivilegedEditor
   ) => void;
-}) {
+}
+
+const Editor = React.forwardRef<ReactQuill,EditorProps>(({
+  onChange,
+  value,
+},ref) =>{
   return (
     <div className="rounded-xl overflow-hidden border-[#d2d2d2] border-2">
       <ReactQuill
@@ -21,7 +22,9 @@ export default function Editor({
         value={value}
         onChange={onChange}
         className="bg-[#edf5f9] dark:!text-black !border-none"
+        ref={ref}
       />
     </div>
   );
-}
+})
+export default Editor;
