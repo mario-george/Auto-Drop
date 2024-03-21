1f723497b895d3485def3061e96616c821aab02b
mario-george <vortexspades@gmail.com>
2024-03-21 15:06:51 +0200

    Refactor Header component to include translation messages and category

--- Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/components/TableComponents/components/Header.tsx
@@ -11,6 +11,8 @@ export default function Header({
   number,
   title,
   className,
+  translationMessages,
+  category,
 }: {
   upProducts: string;
   val: string;
@@ -19,22 +21,44 @@ export default function Header({
   profitType: string;
   title: string;
   className?: string;
+  translationMessages?: {[key:string]:string};
+  category?: string;
 }) {
   const errorButtonRefNoSelection = useRef<HTMLButtonElement>(null);
   const errorButtonRefNoToken = useRef<HTMLButtonElement>(null);
   const errorButtonRefSubmitError = useRef<HTMLButtonElement>(null);
   const successButtonRef = useRef<HTMLButtonElement>(null);
-  
-  const {ErrorComponent:ErrorComponentNoSelection}  = useErrorToast({title:"Error",description:"No product was selected please try again.",errorButtonRef:errorButtonRefNoSelection})
-  const {ErrorComponent:ErrorComponentNoToken}  = useErrorToast({title:"Error",description:"Please link your account with salla and try again.",errorButtonRef:errorButtonRefNoToken})
-  const {ErrorComponent:ErrorComponentSubmitError}  = useErrorToast({title:"Error",description:"Error while linking product.",errorButtonRef:errorButtonRefSubmitError})
-  const {SuccessComponent:SuccessComponent}  = useSuccessToast({title:"Success",description:"Products has been linked successfully.",successButtonRef:successButtonRef})
+
+  const { ErrorComponent: ErrorComponentNoSelection } = useErrorToast({
+    title: "Error",
+    description: "No product was selected please try again.",
+    errorButtonRef: errorButtonRefNoSelection,
+  });
+  const { ErrorComponent: ErrorComponentNoToken } = useErrorToast({
+    title: "Error",
+    description: "Please link your account with salla and try again.",
+    errorButtonRef: errorButtonRefNoToken,
+  });
+  const { ErrorComponent: ErrorComponentSubmitError } = useErrorToast({
+    title: "Error",
+    description: "Error while linking product.",
+    errorButtonRef: errorButtonRefSubmitError,
+  });
+  const { SuccessComponent: SuccessComponent } = useSuccessToast({
+    title: "Success",
+    description: "Products have been linked successfully.",
+    successButtonRef: successButtonRef,
+  });
   const { ProfitComponent, LoaderComponent } = useProfitTypeHandler({
     upProducts,
     val,
     percentage,
     profitType,
-    number,errorButtonRefNoSelection,errorButtonRefNoToken,errorButtonRefSubmitError,successButtonRef
+    number,
+    errorButtonRefNoSelection,
+    errorButtonRefNoToken,
+    errorButtonRefSubmitError,
+    successButtonRef,translationMessages,category
   });
   return (
     <>
@@ -44,11 +68,13 @@ export default function Header({
       {ErrorComponentNoToken}
       {ErrorComponentSubmitError}
       {SuccessComponent}
-      <div className="flex flex-col tab:flex-row justify-between tab:max-w-[90%] px-3 my-3 items-center">
-        <div className={`flex tab:text-[30px] text-[#253439] ${className}`}>
+      <div className="flex flex-col tab:flex-row tab:max-w-[90%] justify-between px-3 my-3 items-center">
+        <div
+          className={`flex tab:text-[30px] text-[#253439] dark:text-white ${className}`}
+        >
           {title}
         </div>{" "}
-        <div>{ProfitComponent}</div>
+        <div className="">{ProfitComponent}</div>
       </div>
     </>
   );
