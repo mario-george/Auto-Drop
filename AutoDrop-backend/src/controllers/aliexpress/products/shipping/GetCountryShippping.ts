e6cd9e1728b2e657f696b9443e73a0a434026444
mario-george <vortexspades@gmail.com>
2024-03-23 16:31:21 +0200

    Add GetShippingProductIdCountryCode function to GetCountryShippping.ts

--- AutoDrop-backend/src/controllers/aliexpress/products/shipping/GetCountryShippping.ts
+++ AutoDrop-backend/src/controllers/aliexpress/products/shipping/GetCountryShippping.ts
@@ -45,3 +45,44 @@ export async function GetProductShippingDetailsByID(
     }
   }
   
+export async function GetShippingProductIdCountryCode(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { product_id ,country_code} = req.body;
+    let user: any = await TokenUserExtractor(req);
+    if (!user) return res.status(401).json({ message: "token is invalid" });
+    console.log("country_code" , country_code)
+    let aliexpressToken = await AliExpressToken.findOne({ userId: user?._id });
+    let tokenInfo = {
+      aliExpressAccessToken: aliexpressToken?.accessToken,
+      aliExpressRefreshToken: aliexpressToken?.refreshToken,
+    };
+
+    /*     if (userType === "vendor")
+        await CheckSubscription(user_id, "products_limit"); */
+
+    const skuid = await GetSKUId({ product_id, tokenInfo });
+    let result = await getProductShippingServices(
+      {
+        sku_id: skuid,
+        country_code,
+        product_id,
+        product_num: "1",
+        price_currency: "SAR",
+      },
+      tokenInfo
+    );
+    
+    // console.log("result",result);
+    if (!result) {
+      result = [];
+    }
+    return res.json({ shipping: result });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
