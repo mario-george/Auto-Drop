50ee1864d8edd823c69d1a2588ea71792b337d93
mario-george <vortexspades@gmail.com>
2024-03-23 12:34:57 +0200

    Add LocationSVG component

---
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/icons/LocationSVG.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function LocationSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="18"
+      fill="none"
+      viewBox="0 0 15 18"
+      className="stroke-red-500 w-[48px] h-[48px]  "
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M7.611 16.972c3.306-3.195 6.611-6.055 6.611-9.583C14.222 3.86 11.262 1 7.612 1 3.96 1 1 3.86 1 7.389c0 3.528 3.306 6.388 6.611 9.583z"
+      className="stroke-black w-[48px] h-[48px]  "
+      ></path>
+    </svg>
+  );
+}
+
+export default LocationSVG;
