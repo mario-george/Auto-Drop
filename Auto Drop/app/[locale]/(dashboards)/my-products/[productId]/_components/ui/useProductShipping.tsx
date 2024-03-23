06a9beed193d74a9c4ca89a4aaa8deb4be3be99f
mario-george <vortexspades@gmail.com>
2024-03-23 12:34:33 +0200

    Add country selection dropdown for shipping location

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/useProductShipping.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/useProductShipping.tsx
@@ -1,16 +1,57 @@
 import { useMemo, useState, useEffect } from "react";
 import CurrencyFormatter from "../../../../products/_components/CurrencyFormatter";
 import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import countries from 'i18n-iso-countries';
+import ar from 'i18n-iso-countries/langs/ar.json';
+import en from 'i18n-iso-countries/langs/en.json';
+import LocationSVG from './icons/LocationSVG';
+import axiosInstance  from '@/app/[locale]/(dashboards)/_components/shared/AxiosInstance';
+
 
 export default function useProductShipping({
   shipping,
   nameOfShippingComp,
   shippingText,
   durationToDeliver,
-  shippingWithoutOrInclude,
+  shippingWithoutOrInclude,to,locale
 }: any) {
   const [value, setValue] = useState("0");
+  const [choosenCountryCode ,setChoosenCountryCode] = useState("SA")
   const [shippingTotalCost, setShippingTotalCost] = useState(0);
+  if (locale === 'ar') {
+    countries.registerLocale(ar);
+  }else{
+    countries.registerLocale(en);
+
+  }
+const shippingMethodsHandler =async ()=>{
+
+  try{
+
+    let res = await axiosInstance.get('/')
+
+    let fetchedShippingMethods = res?.data?.data
+    // setShipping(fetchedShippingMethods)
+  }catch(err){
+    console.error(err)
+  }
+}
+  const countryCodes = Object.keys(countries.getAlpha2Codes());
+  const countryNames = countryCodes.map(code => countries.getName(code, locale));
+  const countriesWithCodes = countryCodes.map((code, index) => ({
+    name: countryNames[index],
+    code: code,
+  }));
+
   useEffect(() => {
     if (shippingWithoutOrInclude == "shippingIncluded") {
       let price = shipping?.[+value]?.freight?.cent / 100;
@@ -26,7 +67,58 @@ export default function useProductShipping({
       {shipping?.length ? (
         <div className="space-y-4 min-w-full  ">
           <div>
+
+            <div className="flex justify-between">
+
             <p className="text-lg font-semibold text-content">{shippingText}</p>
+
+            <div className="flex space-s-9  justify-between items-center">
+
+
+            <LocationSVG/>
+            <p className="text-lg font-semibold text-content">{to}:
+            </p>
+            <Select
+            onValueChange={(value: string) => {
+              /* let valid = true;
+              selectedCategories?.forEach((val: string) => {
+                if (val == value) valid = false;
+              });
+              if (!valid) {
+                return;
+              }
+              console.log("change", value);
+              //@ts-ignore
+              setSelectedCategories((prevCategories: any) => {
+                if (!prevCategories) {
+                  return [value];
+                }
+                return [...prevCategories, value];
+              }); */
+            }}
+            defaultValue={choosenCountryCode}
+          >
+              <SelectTrigger className="  bg-[#edf5f9] text-black text-right text-xs mm:text-sm ml:text-md tab:text-lg">
+              <SelectValue  />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>{'location'}</SelectLabel>
+
+            {countriesWithCodes.map((country)=>{
+              return  <SelectItem
+              key={country.code}
+              value={ country.code}
+              className="z-[50] relative text-xs mm:text-sm ml:text-md tab:text-lg"
+            >
+              {country.name}
+            </SelectItem>
+            })}
+</SelectGroup>
+            </SelectContent>
+            </Select>
+            </div>
+            </div>
             <RadioGroup
               defaultValue="0"
               onChange={setValue}
