ebca3585e4f31205c7007b4c21f2f39ae341e833
mario-george <vortexspades@gmail.com>
2024-03-24 17:13:42 +0200

    Refactor LocationSVG component

--- Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/icons/LocationSVG.tsx
+++ Auto Drop/app/[locale]/(dashboards)/my-products/[productId]/_components/ui/icons/LocationSVG.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function LocationSVG() {
+function Icon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,7 +8,7 @@ function LocationSVG() {
       height="18"
       fill="none"
       viewBox="0 0 15 18"
-      className="stroke-red-500 w-[48px] h-[48px]  "
+      className="w-[24px] h-[24px]"
     >
       <path
         stroke="#fff"
@@ -16,10 +16,18 @@ function LocationSVG() {
         strokeLinejoin="round"
         strokeWidth="1.5"
         d="M7.611 16.972c3.306-3.195 6.611-6.055 6.611-9.583C14.222 3.86 11.262 1 7.612 1 3.96 1 1 3.86 1 7.389c0 3.528 3.306 6.388 6.611 9.583z"
-      className="stroke-black w-[48px] h-[48px]  "
-      ></path>
+     className="stroke-black dark:stroke-white"
+     ></path>
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M7.614 8.986c1.043 0 1.888-.794 1.888-1.775 0-.98-.845-1.774-1.889-1.774-1.043 0-1.888.794-1.888 1.774s.845 1.775 1.889 1.775z"
+     className="stroke-black dark:stroke-white"
+     ></path>
     </svg>
   );
 }
 
-export default LocationSVG;
+export default Icon;
