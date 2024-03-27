921369732df136fdfbd05105deb96cf4de3f6679
mario-george <vortexspades@gmail.com>
2024-03-27 16:49:11 +0200

    Add updateUserSettings route to settings.route.ts

--- AutoDrop-backend/src/routes/settings.route.ts
+++ AutoDrop-backend/src/routes/settings.route.ts
@@ -1,9 +1,10 @@
 import Authentication from "../assits/Authentication"
-import { getUserSettings } from "../controllers/settings"
+import { getUserSettings, updateUserSettings } from "../controllers/settings"
 import express from 'express';
 
 const settingRoute = express.Router()
 
 settingRoute.get('/',[Authentication()],getUserSettings)
+settingRoute.patch('/',[Authentication()],updateUserSettings)
 
 export default settingRoute
