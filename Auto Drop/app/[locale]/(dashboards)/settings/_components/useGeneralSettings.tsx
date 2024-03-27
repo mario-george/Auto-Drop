435705c3e9008b50efe57da23551996fb2253995
mario-george <vortexspades@gmail.com>
2024-03-27 16:48:58 +0200

    Update user authentication logic

--- Auto Drop/app/[locale]/(dashboards)/settings/_components/useGeneralSettings.tsx
+++ Auto Drop/app/[locale]/(dashboards)/settings/_components/useGeneralSettings.tsx
@@ -4,7 +4,7 @@ import { RootState } from "@/store";
 import "./settings.css";
 import { Switch } from "@chakra-ui/react";
 import { Radio, RadioGroup } from "@chakra-ui/react";
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import {
   Select,
   SelectContent,
@@ -15,74 +15,87 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
-import axiosInstance from '@/app/[locale]/(dashboards)/_components/shared/AxiosInstance';
+import axiosInstance from "@/app/[locale]/(dashboards)/_components/shared/AxiosInstance";
+import usePendingToast from "../../../../../components/chakra-ui/usePendingToast";
+import { Button } from "@/components/ui/button";
 interface GeneralSettingsProps {
   locale?: string;
   translation: { [key: string]: string };
+  currWindow: string;
 }
 interface IUserSettings {
-  highestPriceUnion:boolean
-  syncProdPrices:boolean
-  syncProdQuantities:boolean
-  walletAutoPay:boolean
-  shippingType:"withPackeging"
-  originalPriceShipping:"withoutShipping"|"shippingIncluded"
+  highestPriceUnion: boolean;
+  syncProdPrices: boolean;
+  syncProdQuantities: boolean;
+  walletAutoPay: boolean;
+  shippingType: "withPackeging";
+  originalPriceShipping: "withoutShipping" | "shippingIncluded";
 }
 export default function useGeneralSettings(props: GeneralSettingsProps) {
+  const { toastPromiseHandler } = usePendingToast({});
   const [settingsVals, setSettingsVals] = useState<IUserSettings>({
     syncProdPrices: false,
     syncProdQuantities: false,
     walletAutoPay: false,
-    originalPriceShipping:"withoutShipping",
-    shippingType:"withPackeging",
-    highestPriceUnion:false
+    originalPriceShipping: "withoutShipping",
+    shippingType: "withPackeging",
+    highestPriceUnion: false,
   });
 
-  const toggleSwitch = (key:'syncProdPrices'|"syncProdQuantities"|"highestPriceUnion"|"walletAutoPay")=>{
-/*     if(typeof settingsVals[key]!=="boolean"){
+  const toggleSwitch = (
+    key:
+      | "syncProdPrices"
+      | "syncProdQuantities"
+      | "highestPriceUnion"
+      | "walletAutoPay"
+  ) => {
+    /*     if(typeof settingsVals[key]!=="boolean"){
       return
     } */
-    setSettingsVals((prevSettings)=>{
-      return {...prevSettings,
-      [key]:!settingsVals[key]
-    }
-    })
-  }
-  const saveSettingsHandler = async()=>{
-    let res = await axiosInstance.patch("/settings",settingsVals)
-    
-    if(res.status<=200 && res.status<300){
+    setSettingsVals((prevSettings) => {
+      return { ...prevSettings, [key]: !settingsVals[key] };
+    });
+  };
+  console.log("settingsVals", settingsVals);
+
+  const saveSettingsHandler = async () => {
+    console.log("settingsVals", settingsVals);
+    let res = axiosInstance.patch("/settings", settingsVals);
+    let resData = await res;
+    toastPromiseHandler({
+      promise: res,
+      loadingTitle: "Saving settings",
+      successTitle: "Settings saved",
+      loadingDescription: "Please wait",
+      descriptionSuccess: "Settings saved successfully",
+      loadingPosition: "bottom-right",isClosable:true
+    });
+    if (resData.status >= 200 && resData.status < 300) {
       // toast success
-      console.log("settings saved")
-    }else{
+      console.log("settings saved");
+    } else {
       // toast save error
     }
-    
-
-  }
-useEffect(()=>{
-  try{
-    const fetchSettings = async()=>{
-
-      const res = await axiosInstance.get("/settings")
-let userSettings = res.data.data
-console.log("userSettings",userSettings)
-      setSettingsVals((prevSettings)=>{
-        return {
-          ...prevSettings,userSettings
-        }
-      })
+  };
+  useEffect(() => {
+    try {
+      const fetchSettings = async () => {
+        const res = await axiosInstance.get("/settings");
+        let userSettings = res.data.data;
+        console.log("userSettings", userSettings);
+        setSettingsVals((prevSettings) => {
+          return {
+            ...prevSettings,
+            ...userSettings,
+          };
+        });
+      };
+      fetchSettings();
+    } catch (err) {
+      console.error(err);
     }
-    fetchSettings()
-  }catch(err){
-    console.error(err)
-  }
-},[])
-  const {
-    locale,
-
-    translation,
-  } = props;
+  }, []);
+  const { locale, translation, currWindow } = props;
 
   let {
     generalSettings,
@@ -105,146 +118,157 @@ console.log("userSettings",userSettings)
     orderSettings,
     sa,
   } = translation;
-let {  syncProdPrices ,  } = settingsVals
-  let GeneralSettingsComponent =  <>
-  <MotionWrapper locale={locale}>
-    <div className="ms:text-sm flex flex-col space-y-3 tab:space-y-6 mt-3">
-      <div className="flex justify-center tab:justify-end  max-w-[90%] tab:max-w-[60%] "></div>
-      <div className="boldHeader"> {productSettings}</div>
-
-      <div className="generalSettingsField   ">
-        <Switch checked={true} onChange={()=>{toggleSwitch("syncProdPrices")}} />
-        <div>{syncProductPrices}</div>
-      </div>
-      <div className="generalSettingsField   ">
-        <Switch checked={true} onChange={()=>{toggleSwitch("syncProdQuantities")}}/>
-        <div>{syncProductQuantities}</div>
-      </div>
-      <div className="boldHeader">{productPricingSettings}</div>
-      <div className="generalSettingsField  ">
-        <Switch checked={true} onChange={()=>{toggleSwitch("highestPriceUnion")}} />
-        <div>{consolidatePricing}</div>
-      </div>
-      <div className="generalSettingsLargeField  lap:!space-s-16 tab:!max-w-[70%] lap:!max-w-[35%]  ">
-        <div className="tab:whitespace-nowrap">{viewOriginal}</div>
-        <RadioGroup
-          // defaultValue="shippingIncluded"
-          className="grid grid-cols-1 ml:grid-cols-2 gap-2 tab:my-0 my-2 ml:my-3 w-full"
-          /*               onValueChange={(value: string) => {
-                  setShippingWithoutOrInclude(value);
-                }} */
-          /*  onChange={(value: string) => {
-                  setShippingWithoutOrInclude(value);
-                }} */
-          // value={shippingWithoutOrInclude}
+  let {
+    syncProdPrices,
+    syncProdQuantities,
+    walletAutoPay,
+    highestPriceUnion,
+    originalPriceShipping,
+  } = settingsVals;
+  let SaveGeneralSettingsButton = (
+    <>
+      {currWindow == "GeneralSettings" ? (
+        <Button
+          className="ms:max-w-[50%] ms:my-4  tab:max-w-[12rem] bg-[#253439] rounded-lg  hover:bg-[#253439] tab:mx-auto dark:bg-white dark:text-[#2E464F] "
+          onClick={saveSettingsHandler}
         >
-          <div className="flex items-center space-s-2  bg-[#edf5f9] p-2 rounded-md">
-            <Radio value="withoutShipping">
-              <div
-                className=" text-xs dark:text-black whitespace-nowrap"
-                // htmlFor="r11"
-              >
-                {withoutShipping}
-              </div>
-            </Radio>
-          </div>
-          <div className="flex items-center space-x-2 bg-[#edf5f9] p-2 rounded-md">
-            <Radio value="shippingIncluded">
-              <div
-                className="text-xs  dark:text-black whitespace-nowrap"
-                // htmlFor="r22"
-              >
-                {includedShipping}
-              </div>
-            </Radio>
-          </div>
-        </RadioGroup>
-      </div>
+          {save}
+        </Button>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+  let GeneralSettingsComponent = (
+    <>
+      <MotionWrapper locale={locale}>
+        <div className="ms:text-sm flex flex-col space-y-3 tab:space-y-6 mt-3">
+          <div className="flex justify-center tab:justify-end  max-w-[90%] tab:max-w-[60%] "></div>
+          <div className="boldHeader"> {productSettings}</div>
 
-      <div className="boldHeader">{shippingSettings}</div>
-
-      <div className="flex flex-col space-y-3 tab:max-w-[30%]">
-        <div>{shippingType}</div>
-        <Select
-          onValueChange={(value: any) => {
-            // setProfitChoosenType(value);
-          }}
-          value={shippedW}
-        >
-          <SelectTrigger className="bg-[#edf5f9] dark:text-black">
-            <SelectValue
-              className=" dark:text-[#253439]"
-              placeholder={shippedW}
+          <div className="generalSettingsField   ">
+            <Switch
+              isChecked={syncProdPrices}
+              onChange={() => {
+                toggleSwitch("syncProdPrices");
+              }}
             />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value={shippedW}>{shippedW}</SelectItem>
-              {/* <SelectItem value="percentage">{'percentage'}</SelectItem> */}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+            <div>{syncProductPrices}</div>
+          </div>
+          <div className="generalSettingsField   ">
+            <Switch
+              isChecked={syncProdQuantities}
+              onChange={() => {
+                toggleSwitch("syncProdQuantities");
+              }}
+            />
+            <div>{syncProductQuantities}</div>
+          </div>
+          <div className="boldHeader">{productPricingSettings}</div>
+          <div className="generalSettingsField  ">
+            <Switch
+              isChecked={highestPriceUnion}
+              onChange={() => {
+                toggleSwitch("highestPriceUnion");
+              }}
+            />
+            <div>{consolidatePricing}</div>
+          </div>
+          <div className="generalSettingsLargeField  lap:!space-s-16 tab:!max-w-[70%] lap:!max-w-[35%]  ">
+            <div className="tab:whitespace-nowrap">{viewOriginal}</div>
+            <RadioGroup
+              // defaultValue="shippingIncluded"
+              className="grid grid-cols-1 ml:grid-cols-2 gap-2 tab:my-0 my-2 ml:my-3 w-full"
+              onChange={(value: "shippingIncluded"|"withoutShipping") => {
 
-      <Separator />
-      <div className="flex flex-col space-y-2">
-        <div className="">{shippedW}</div>
-        <Separator />
-        <div className="flex justify-between tab:max-w-[25%]">
-          <div className="">{paiementWhenRecieving}</div>
-          <div className="text-[#C1121F]">5{sa}</div>
+                
+                setSettingsVals((prevSettings:IUserSettings) => {
+                  return { ...prevSettings, originalPriceShipping: value };
+                });
+              }}
+              value={originalPriceShipping}
+            >
+              <div className="flex items-center space-s-2  bg-[#edf5f9] p-2 rounded-md">
+                <Radio value="withoutShipping">
+                  <div
+                    className=" text-xs dark:text-black whitespace-nowrap"
+                    // htmlFor="r11"
+                  >
+                    {withoutShipping}
+                  </div>
+                </Radio>
+              </div>
+              <div className="flex items-center space-x-2 bg-[#edf5f9] p-2 rounded-md">
+                <Radio value="shippingIncluded">
+                  <div
+                    className="text-xs  dark:text-black whitespace-nowrap"
+                    // htmlFor="r22"
+                  >
+                    {includedShipping}
+                  </div>
+                </Radio>
+              </div>
+            </RadioGroup>
+          </div>
+
+          <div className="boldHeader">{shippingSettings}</div>
+
+          <div className="flex flex-col space-y-3 tab:max-w-[30%]">
+            <div>{shippingType}</div>
+            <Select
+              onValueChange={(value: any) => {
+                // setProfitChoosenType(value);
+              }}
+              value={shippedW}
+            >
+              <SelectTrigger className="bg-[#edf5f9] dark:text-black">
+                <SelectValue
+                  className=" dark:text-[#253439]"
+                  placeholder={shippedW}
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value={shippedW}>{shippedW}</SelectItem>
+                  {/* <SelectItem value="percentage">{'percentage'}</SelectItem> */}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <Separator />
+          <div className="flex flex-col space-y-2">
+            <div className="">{shippedW}</div>
+            <Separator />
+            <div className="flex justify-between tab:max-w-[25%]">
+              <div className="">{paiementWhenRecieving}</div>
+              <div className="text-[#C1121F]">5{" "+sa}</div>
+            </div>
+
+            <Separator />
+            <div className="flex justify-between tab:max-w-[25%]">
+              <div className="">{delAndPack}</div>
+
+              <div className="text-[#C1121F]">10{" " + sa}</div>
+            </div>
+            <Separator />
+          </div>
+
+          <div className="text-[#C1121F]">{pricesVAT}</div>
+          <div className="boldHeader">{orderSettings}</div>
+          <div className="generalSettingsField   ">
+            <Switch
+              className=""
+              isChecked={walletAutoPay}
+              onChange={() => {
+                toggleSwitch("walletAutoPay");
+              }}
+            />
+            <div>{autoPay}</div>
+          </div>
         </div>
-
-        <Separator />
-        <div className="flex justify-between tab:max-w-[25%]">
-          <div className="">{delAndPack}</div>
-
-          <div className="text-[#C1121F]">10{" " + sa}</div>
-        </div>
-        <Separator />
-      </div>
-
-      <div className="text-[#C1121F]">{pricesVAT}</div>
-      <div className="boldHeader">{orderSettings}</div>
-      <div className="generalSettingsField   ">
-        <Switch className="" checked={true} onChange={()=>{toggleSwitch("walletAutoPay")}} />
-        <div>{autoPay}</div>
-      </div>
-
-      {/*  <div className="generalSettingsField   ">
-        <div>{merchantID}</div>
-        <Input
-          className="inputFieldDisabled"
-          defaultValue={userId}
-          disabled
-        />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{name}</div>
-        <Input className="inputField" value={userName} />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{marketName}</div>
-        <Input className="inputField" />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{marketLink}</div>
-        <Input className="inputField" />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{email}</div>
-        <Input className="inputField" value={userEmail} />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{phone}</div>
-        <Input className="inputField" value={userPhone} />
-      </div>
-      <div className="generalSettingsField  ">
-        <div>{country}</div>
-        <Input className="inputField" value={userCountry} />
-      </div> */}
-    </div>
-  </MotionWrapper>
-</>
-  return {GeneralSettingsComponent,settingsVals};
+      </MotionWrapper>
+    </>
+  );
+  return { GeneralSettingsComponent, settingsVals, SaveGeneralSettingsButton };
 }
