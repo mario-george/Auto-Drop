8a4f8c7aa983de52a74df48196eda061a5149611
mario-george <vortexspades@gmail.com>
2024-03-27 16:49:06 +0200

    Refactor usePendingToast function to accept custom toast properties

--- Auto Drop/components/chakra-ui/usePendingToast.tsx
+++ Auto Drop/components/chakra-ui/usePendingToast.tsx
@@ -9,6 +9,22 @@ interface IPendingToast {
     | "top"
     | "bottom";
 }
+interface IPromiseHandler {
+  promise?: Promise<any>;
+  loadingPosition?:
+    | "bottom-right"
+    | "bottom-left"
+    | "top-right"
+    | "top-left"
+    | "top"
+    | "bottom";
+    successTitle:string
+    
+    loadingTitle:string
+    loadingDescription:string
+    descriptionSuccess:string
+    isClosable:boolean
+}
 function usePendingToast({
   promise = new Promise((resolve, reject) => {
     setTimeout(() => resolve(200), 5000);
@@ -16,23 +32,38 @@ function usePendingToast({
   position = "bottom-right",
 }: IPendingToast) {
   const toast = useToast();
-  return (
-    <Button
-      onClick={() => {
-        toast.promise(promise, {
-          // position: 'bottom-right',
-          success: { title: "Promise resolved", description: "Looks great" },
-          error: { title: "Promise rejected", description: "Something wrong" },
-          loading: {
-            title: "Promise pending",
-            description: "Please wait",
-            position: position,
-          },
-        });
-      }}
-    >
-      Show Toast
-    </Button>
-  );
+
+  const toastPromiseHandler = ({promise= new Promise((resolve, reject) => {
+    setTimeout(() => resolve(200), 5000);
+  }),loadingPosition,successTitle="Success",descriptionSuccess="Process done successfully.",loadingTitle="Loading",loadingDescription="Please Wait",isClosable=false}:IPromiseHandler)=>{
+    toast.promise(promise, {
+      // position: 'bottom-right',
+      success: { title: successTitle, description:descriptionSuccess,isClosable},
+      error: { title: "Promise rejected", description: "Something wrong",isClosable },
+      loading: {
+        title: loadingTitle,
+        description: loadingDescription,
+        position: loadingPosition,isClosable
+      },
+    });
+  }
+  let ToastButtonComponent =  <Button
+  onClick={() => {
+    toast.promise(promise, {
+      // position: 'bottom-right',
+      success: { title: "Promise resolved", description: "Looks great" },
+      error: { title: "Promise rejected", description: "Something wrong" },
+      loading: {
+        title: "Promise pending",
+        description: "Please wait",
+        position: position,
+      },
+    });
+  }}
+>
+  Show Toast
+</Button>
+
+  return {toastPromiseHandler,ToastButtonComponent}
 }
 export default usePendingToast;
