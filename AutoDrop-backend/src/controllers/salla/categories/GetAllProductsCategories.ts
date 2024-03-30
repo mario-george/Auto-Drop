1131d69a57de665e49124bc24fe851709fc99115
mario-george <vortexspades@gmail.com>
2024-03-30 21:01:31 +0200

    Refactor GetAllProductsCategories controller to exclude _id field in categoriesSalla

--- AutoDrop-backend/src/controllers/salla/categories/GetAllProductsCategories.ts
+++ AutoDrop-backend/src/controllers/salla/categories/GetAllProductsCategories.ts
@@ -6,25 +6,8 @@ import { Product } from "../../../models/product.model";
 
 export const GetAllProductsCategories = catchAsync(
   async (req: Request & any, res: Response, next: NextFunction) => {
-  /*   const salla = await SallaToken.findById(req.user.sallaToken);
-    if (!salla) {
-        return res.status(404).json({ message: 'Salla token not found' });
-      } */
-  
-    // const { accessToken } = salla;
-   /*  const categoriesFetch = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-      url: "https://api.salla.dev/admin/v2/categories",
-    }; */
-    // const categories = await axios.request(categoriesFetch);
-    // console.log(categories.data);
-    // let categoriesData= categories.data.data.map((category: any) => {let{id,name}=category;return {name,id}})
-
- let productsCategories = await Product.find({merchant:req.user._id.toString()}).select("categoriesSalla")    
+ 
+ let productsCategories = await Product.find({merchant:req.user._id.toString()}).select("categoriesSalla -_id")    
     res.status(200).json({
       status: "success",
       data: productsCategories,
