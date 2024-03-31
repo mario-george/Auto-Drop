3fe7d8041b5deca3ae9e4a6b139f3ee12ea85eef
mario-george <vortexspades@gmail.com>
2024-03-31 21:30:17 +0200

    Add OrderDetailsFetch component

---
+++ Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/OrderDetailsFetch.tsx
@@ -0,0 +1,20 @@
+import { useSearchParams } from 'next/navigation';
+import useEffect from 'react';
+import axiosInstance from '@/app/[locale]/(dashboards)/_components/shared/AxiosInstance';
+import useState from 'react';
+import useOrderDetails from './useOrderDetails';
+import useOrderRenderer from './useOrderRenderer';
+"use client"
+interface OrderFetchProps {
+    translationMessages: {[key:string]:string};
+
+}
+export default function OrderFetch({translationMessages}:OrderFetchProps){
+const searchParams = useSearchParams();
+const orderId = searchParams.get('orderId') as string
+const  {orderData} = useOrderDetails({orderId})
+const { OrderDataComponent } = useOrderRenderer({orderData,translationMessages})
+return <>OrderFetch
+{OrderDataComponent}</>
+
+}
