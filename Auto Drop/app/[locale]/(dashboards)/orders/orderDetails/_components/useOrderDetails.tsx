cc13695984927d7b227dd86a83030881c6d4d69e
mario-george <vortexspades@gmail.com>
2024-03-31 21:31:25 +0200

    Add useOrderDetails hook for fetching order data

---
+++ Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/useOrderDetails.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axiosInstance from "../../../_components/shared/AxiosInstance";
+
+interface OrderDetailsProps {
+  orderId: string | number;
+}
+interface OrderData{
+
+
+
+}
+
+export default function useOrderDetails({ orderId }: OrderDetailsProps) {
+  const [orderData, setOrderData] = useState<OrderData |string |OrderData[]>([]);
+
+  useEffect(() => {
+    const orderFetchHandler = async () => {
+      if (orderId) {
+        const res = await axiosInstance.get("/order/" + orderId);
+        let { data } = res;
+        setOrderData(data);
+        if (res.status >= 200 && res.status < 300) {
+          console.log(data);
+        } else {
+          console.log("Error");
+          setOrderData('fail')
+        }
+      }
+    };
+    orderFetchHandler();
+  }, [orderId]);
+  return { orderData };
+}
