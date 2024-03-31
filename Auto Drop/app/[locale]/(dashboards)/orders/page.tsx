e5a033d3c84076f579227174e281074e40895dca
mario-george <vortexspades@gmail.com>
2024-03-31 21:30:01 +0200

    Refactor translation messages and fix formatting in Orders page

--- Auto Drop/app/[locale]/(dashboards)/orders/page.tsx
+++ Auto Drop/app/[locale]/(dashboards)/orders/page.tsx
@@ -6,27 +6,26 @@ import "./components/styles/styles.css";
 
 export default function Orders() {
   const t = useTranslations("ordersPage");
-const locale = useLocale()
-const translationMessages = {
-  "orders":t("orders"),
-  "sender":t("sender"),
-  "orderNumber":t("orderNumber"),
-  "orderStatus":t("orderStatus"),
-  "date":t("date"),
-  "orderSource":t("orderSource"),
-  "amount":t("amount")
-
-}
-let HeaderProps = {
-  translationMessages
-}
-let TableRendererProps ={translationMessages}
-  return <>
-  <div className="px-3">
-  <Header {...HeaderProps} className="w-fit" />
-      <TableRenderer
-   {...TableRendererProps}
-      />
+  const locale = useLocale();
+  const translationMessages = {
+    orders: t("orders"),
+    sender: t("sender"),
+    orderNumber: t("orderNumber"),
+    orderStatus: t("orderStatus"),
+    date: t("date"),
+    orderSource: t("orderSource"),
+    amount: t("amount"),
+  };
+  let HeaderProps = {
+    translationMessages,
+  };
+  let TableRendererProps = { translationMessages };
+  return (
+    <>
+      <div className="px-3">
+        <Header {...HeaderProps} className="w-fit" />
+        <TableRenderer {...TableRendererProps} />
       </div>
-  </>;
+    </>
+  );
 }
