0b3db17d472492cfcbd8fa7e92525c518075ff95
mario-george <vortexspades@gmail.com>
2024-04-03 04:27:16 +0200

    Add order shipping details and product details to OrderFetch component

--- Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/OrderDetailsFetch.tsx
+++ Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/OrderDetailsFetch.tsx
@@ -5,20 +5,52 @@ import axiosInstance from "@/app/[locale]/(dashboards)/_components/shared/AxiosI
 import useState from "react";
 import useOrderDetails from "./useOrderDetails";
 import useOrderRenderer from "./useOrderRenderer";
+import useOrderDetailsShipping from "./ui/useOrderDetailsShipping";
 interface OrderFetchProps {
   translationMessages: { [key: string]: string };
+  locale:string
 }
-export default function OrderFetch({ translationMessages }: OrderFetchProps) {
+export default function OrderFetch({ translationMessages,locale }: OrderFetchProps) {
   const searchParams = useSearchParams();
   const orderId = searchParams.get("orderId") as string;
-  const { orderData } = useOrderDetails({ orderId });
+  const { orderData ,ProductDetails} = useOrderDetails({ orderId ,translationMessages,locale});
   const { OrderDataComponent } = useOrderRenderer({
     orderData,
     translationMessages,
   });
+let {   shipping:shippingText,
+  shippingType,
+  withLogo,
+  attachALogo,
+  shippedWPack,
+  packagingBag,
+  cartoon,
+  attachAnInvoice,
+  placeALogo,
+  supplierShipping,
+  estimatedDuration,
+  shippingCompanyName, price,withInvoice} = translationMessages
+let OrderShippingProps = {
+  shippingText,
+  shippingType,
+  withLogo,
+  attachALogo,
+  shippedWPack,
+  packagingBag,
+  cartoon,
+  attachAnInvoice,
+  placeALogo,
+  supplierShipping,
+  estimatedDuration,
+  shippingCompanyName,
+  locale,price,withInvoice
+}
+  const {OrderShipping} = useOrderDetailsShipping({...OrderShippingProps}) 
   return (
     <>
       OrderFetch
+      {ProductDetails}
+      {OrderShipping}
       {OrderDataComponent}
     </>
   );
