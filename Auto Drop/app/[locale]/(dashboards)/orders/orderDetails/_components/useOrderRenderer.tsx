ddd743298b40aade368fb970a74f1d81a0cf4ff7
mario-george <vortexspades@gmail.com>
2024-04-03 04:27:47 +0200

    Refactor useOrderRenderer function

--- Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/useOrderRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/useOrderRenderer.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Spinner } from '@chakra-ui/react';
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
 
-export default function useOrderRenderer({orderData,translationMessages}: { orderData: any;translationMessages: {[key:string]:string};}){
-    let OrderDataComponent = <> </>
-    let {orderDetails} = translationMessages
-if(orderData == "fail"){
-    OrderDataComponent = <>Order not found</>
-}else if (orderData.length == 0){
-    OrderDataComponent =  <div className="flex space-s-3 items-center text-green-500">
-    <Spinner />
-    <div className="dark:text-white">Fetching Order...</div>
-  </div>
-}else if(orderData.length > 0){
-OrderDataComponent = <>{orderDetails}</>
-}
+export default function useOrderRenderer({
+  orderData,
+  translationMessages,
+}: {
+  orderData: any;
+  translationMessages: { [key: string]: string };
+}) {
+  let OrderDataComponent = <> </>;
+  let { orderDetails } = translationMessages;
+  if (orderData == "fail") {
+    OrderDataComponent = <>Order not found</>;
+  } else if (!orderData) {
+    OrderDataComponent = (
+      <div className="flex space-s-3 items-center text-green-500">
+        <Spinner />
+        <div className="dark:text-white">Fetching Order...</div>
+      </div>
+    );
+  } else if (orderData) {
+    OrderDataComponent = <>{orderDetails}</>;
+  }
 
-  return {OrderDataComponent}
+  return { OrderDataComponent };
 }
