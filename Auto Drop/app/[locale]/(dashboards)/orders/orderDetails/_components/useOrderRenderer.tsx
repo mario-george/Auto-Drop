37a9100c2045b032ffc4ccf6e4f24c95be2dd1d8
mario-george <vortexspades@gmail.com>
2024-04-03 16:49:11 +0200

    Add useOrderDetailsNotes and useOrderDetailsPayment components

--- Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/useOrderRenderer.tsx
+++ Auto Drop/app/[locale]/(dashboards)/orders/orderDetails/_components/useOrderRenderer.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Spinner } from "@chakra-ui/react";
+import useOrderDetailsNotes from "./ui/useOrderDetailsNotes";
+import useOrderDetailsPayment from "./ui/useOrderDetailsPayment";
 
 export default function useOrderRenderer({
   orderData,
-  translationMessages,
+  translationMessages,locale,commentsText
 }: {
   orderData: any;
   translationMessages: { [key: string]: string };
+  locale:string
+  commentsText:string
 }) {
+  const [successLoadedOrder,setSuccessLoadedOrder] = useState(false)
+  let merchantStore = orderData?.urls?.customer ?? ""
+  let { paymentProcess,cod,total,paymentFromWallet,payNow, } = translationMessages;
+  console.log("orderData",orderData)
+  const {OrderNotes} = useOrderDetailsNotes({commentsText,merchantStore:orderData?.customer?.urls?.customer ?? "",locale})
+  let OrderDetailsPaymentProps = {
+    paymentProcess,cod,total,paymentFromWallet,payNow, 
+  }
+   const {OrderPayment} = useOrderDetailsPayment({...OrderDetailsPaymentProps,locale,totalPrice:orderData?.totalPrice||0})
+ useEffect(()=>{
+  if(!successLoadedOrder && typeof orderData == "object"){
+
+    setSuccessLoadedOrder(true)
+  }
+ },[orderData])
   let OrderDataComponent = <> </>;
-  let { orderDetails } = translationMessages;
   if (orderData == "fail") {
     OrderDataComponent = <>Order not found</>;
   } else if (!orderData) {
@@ -20,8 +38,11 @@ export default function useOrderRenderer({
       </div>
     );
   } else if (orderData) {
-    OrderDataComponent = <>{orderDetails}</>;
+   
+    OrderDataComponent = <>
+    {OrderNotes}
+    {OrderPayment}</>;
   }
 
-  return { OrderDataComponent };
+  return { OrderDataComponent ,successLoadedOrder};
 }
