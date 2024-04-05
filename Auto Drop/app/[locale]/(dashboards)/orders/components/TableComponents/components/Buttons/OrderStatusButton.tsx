3588bc7205a2b2f4ade9b7f0dacca53b8fd49b79
mario-george <vortexspades@gmail.com>
2024-04-05 14:05:22 +0200

    Add localization support for order status buttons

--- Auto Drop/app/[locale]/(dashboards)/orders/components/TableComponents/components/Buttons/OrderStatusButton.tsx
+++ Auto Drop/app/[locale]/(dashboards)/orders/components/TableComponents/components/Buttons/OrderStatusButton.tsx
@@ -6,15 +6,23 @@ export default function OrderStatusButton({
 }: {
   orderStatus: string;
 }) {
+    let deliveryProgressStrings  = ["جاري التوصيل","Delivery in progess"]
+  let deliveredStrings  = ["تم التوصيل","Delivered"]
+  let InReveiwStrings  = ["قيد المراجعة","In Review"]
+  let InProgressStrings  = ["قيد التنفيذ","In Progress"]
+  let createdStrings  = ["تم الانشاء","Created"]
+  let rejectedStrings  = ["ملغي","Rejected"]
   let buttonC = " rounded-lg px-3 h-8";
-  if (orderStatus === "قيد الدفع") {
-    buttonC += " text-[#253439] bg-[#eaebec] dark:bg-gray-300 dark:text-black";
-  } else if (orderStatus === "مكتمل") {
-    buttonC += " text-[#008767] bg-[#f2fcfb] dark:bg-green-300 px-5 dark:text-black";
-  } else {
-    buttonC += " text-[#c1121f] bg-[#ffd6d9] dark:bg-red-300 px-6 dark:text-black";
-  }
+  if (createdStrings.includes(orderStatus)) { 
+    buttonC += " text-[#253439] bg-[#FACC15] dark:text-black";
+  } else if (InReveiwStrings.includes(orderStatus)) {
+    buttonC += " text-[#008767] bg-[#916F16] px-5 dark:text-black";
+  } else if(InProgressStrings.includes(orderStatus)) {
+    buttonC += " text-[#c1121f] bg-[#3F51B5] px-6 dark:text-black";
+  }else{
+    buttonC += " text-[#c1121f] bg-[#ffd6d9] px-6 dark:text-black";
 
+  }
   return (
     <div>
       <Button className={buttonC}>{orderStatus}</Button>
