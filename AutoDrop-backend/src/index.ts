11d2b5819ec58f521fa90cf2b01ddfd8fd16f705
mario-george <vortexspades@gmail.com>
2024-04-10 04:48:13 +0200

    Remove DefaultDocChecker from index.ts and add it to DBConnection.ts

--- AutoDrop-backend/src/index.ts
+++ AutoDrop-backend/src/index.ts
@@ -26,7 +26,6 @@ import WebHookHandler from "./controllers/Webhook/WebHookHandler";
 import orderRoutes from './routes/order.routes';
 import TokenRefreshHandler from "./cron/aliexpress/tokens/TokenRefreshHandler";
 import ProductUpToDate from "./cron/aliexpress";
-import DefaultDocChecker from './validate/DefaultDocChecker';
 const app = express();
 
 //Parse json bodies
@@ -119,7 +118,6 @@ app.use(globalErrorHandler);
 
 app.listen(10000, () => {
   console.log(`server is running `);
-  DefaultDocChecker()
   TokenRefreshHandler.start()
   ProductUpToDate.start()
 });
