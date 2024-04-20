ca41905446c17d84ec65267ac3a7810646f776ab
mario-george <vortexspades@gmail.com>
2024-04-20 13:16:32 +0200

    Add EditCustomerData route to order.routes.ts

--- AutoDrop-backend/src/routes/order.routes.ts
+++ AutoDrop-backend/src/routes/order.routes.ts
@@ -1,4 +1,5 @@
 import Authentication from "../assits/Authentication";
+import { EditCustomerData } from "../controllers/orders/EditCustomerData";
 
 import { GetUserOrders,GetUserOrderDetails } from "../controllers/orders/GetUserOrders";
 import { SendOrder } from "../controllers/orders/PlaceOrder";
@@ -8,7 +9,7 @@ const orderRoutes = Router();
 orderRoutes.get("/getOrder", [Authentication()], GetUserOrders);
 orderRoutes.post("/getOrderDetails", [Authentication()], GetUserOrderDetails);
 orderRoutes.post("/sendOrder", [Authentication()], SendOrder);
-
+orderRoutes.patch("/editCustomer", [Authentication()], EditCustomerData);
 
 
 
