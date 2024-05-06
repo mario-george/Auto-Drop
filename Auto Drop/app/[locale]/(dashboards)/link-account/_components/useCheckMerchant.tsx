c6115cbe62d2e8731950057388a8f66f6dc8a578
mario-george <vortexspades@gmail.com>
2024-05-06 21:42:58 +0300

    Refactor WebSocket connection logic and handle connection errors

--- Auto Drop/app/[locale]/(dashboards)/link-account/_components/useCheckMerchant.tsx
+++ Auto Drop/app/[locale]/(dashboards)/link-account/_components/useCheckMerchant.tsx
@@ -2,28 +2,32 @@
 
 import { useToast } from "@chakra-ui/react"
 import { useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 
 export default function useCheckMerchant(){
 let params = useSearchParams()
-let merchantAlreadyConnected=params.get("?merchantAlreadyConnected=true")
+let merchantAlreadyConnected=params.get("merchantAlreadyConnected")
 let error = false
+console.log(merchantAlreadyConnected)
 if( merchantAlreadyConnected=='true'){
     error = true
 }
 const toast = useToast()
 let title="Error"
 let description="Merchant already connected"
+useEffect(()=>{
+    if(error){
+        toast({
+            title,
+            description,
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+            position: "bottom-right",
+          });
+    }
+},[error])
 
-if(error){
-    toast({
-        title,
-        description,
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-        position: "bottom-right",
-      });
-}
 return {error}
 
 }
